c7d96754eadc0f2716113f88b87df436cfcf1dac
InDieTasten <indietasten@gmail.com>
2014-08-02 17:03:19 +0200

    SFML-Recompilation state 3

---
+++ sources/Sound.cpp
@@ -0,0 +1,27 @@
+#include "Sound.hpp"
+//CONSTRUCTORS
+Sound::Sound(std::string _id, std::string _path)
+{
+
+}
+//DESTRUCTORS
+
+//METHODS
+void Sound::load()
+{
+
+}
+void Sound::unload()
+{
+
+}
+sf::SoundBuffer* Sound::getSound()
+{
+	if(sound.size() == 1)
+	{
+		return &(*sound.begin());
+	} else {
+		load();
+		return &(*sound.begin());
+	}
+}
