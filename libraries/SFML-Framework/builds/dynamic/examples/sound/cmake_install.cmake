c4b3872f1f997609d8366c1db163aaeabc648bb6
InDieTasten <indietasten@gmail.com>
2014-08-03 22:29:51 +0200

    SFML(Master)- Implementation + Release-Compilation
    Former-commit-id: 7493d291a8d0da5afd65c7fe33bac266eef7db93

---
+++ libraries/SFML-Framework/builds/dynamic/examples/sound/cmake_install.cmake
@@ -0,0 +1,47 @@
+# Install script for directory: C:/Users/InDie/Bibliotheken/Documents/GitHub/--UntitledGame--/libraries/SFML-Framework/examples/sound
+
+# Set the install prefix
+if(NOT DEFINED CMAKE_INSTALL_PREFIX)
+  set(CMAKE_INSTALL_PREFIX "C:/Program Files/SFML")
+endif()
+string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
+
+# Set the install configuration name.
+if(NOT DEFINED CMAKE_INSTALL_CONFIG_NAME)
+  if(BUILD_TYPE)
+    string(REGEX REPLACE "^[^A-Za-z0-9_]+" ""
+           CMAKE_INSTALL_CONFIG_NAME "${BUILD_TYPE}")
+  else()
+    set(CMAKE_INSTALL_CONFIG_NAME "Release")
+  endif()
+  message(STATUS "Install configuration: \"${CMAKE_INSTALL_CONFIG_NAME}\"")
+endif()
+
+# Set the component getting installed.
+if(NOT CMAKE_INSTALL_COMPONENT)
+  if(COMPONENT)
+    message(STATUS "Install component: \"${COMPONENT}\"")
+    set(CMAKE_INSTALL_COMPONENT "${COMPONENT}")
+  else()
+    set(CMAKE_INSTALL_COMPONENT)
+  endif()
+endif()
+
+if(NOT CMAKE_INSTALL_COMPONENT OR "${CMAKE_INSTALL_COMPONENT}" STREQUAL "examples")
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/./examples/sound" TYPE EXECUTABLE FILES "C:/Users/InDie/Bibliotheken/Documents/GitHub/--UntitledGame--/libraries/SFML-Framework/builds/dynamic/examples/sound/sound.exe")
+  if(EXISTS "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/./examples/sound/sound.exe" AND
+     NOT IS_SYMLINK "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/./examples/sound/sound.exe")
+    if(CMAKE_INSTALL_DO_STRIP)
+      execute_process(COMMAND "C:/Program Files/CodeBlocks/MinGW/bin/strip.exe" "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/./examples/sound/sound.exe")
+    endif()
+  endif()
+endif()
+
+if(NOT CMAKE_INSTALL_COMPONENT OR "${CMAKE_INSTALL_COMPONENT}" STREQUAL "examples")
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/./examples/sound" TYPE FILE FILES "C:/Users/InDie/Bibliotheken/Documents/GitHub/--UntitledGame--/libraries/SFML-Framework/examples/sound/Sound.cpp")
+endif()
+
+if(NOT CMAKE_INSTALL_COMPONENT OR "${CMAKE_INSTALL_COMPONENT}" STREQUAL "examples")
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/./examples/sound" TYPE DIRECTORY FILES "C:/Users/InDie/Bibliotheken/Documents/GitHub/--UntitledGame--/libraries/SFML-Framework/examples/sound/resources")
+endif()
+
---
+++ libraries/SFML-Framework/builds/static/examples/sockets/CMakeFiles/sockets.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sockets.dir/Sockets.cpp.obj"
+  "CMakeFiles/sockets.dir/TCP.cpp.obj"
+  "CMakeFiles/sockets.dir/UDP.cpp.obj"
+  "sockets.pdb"
+  "sockets.exe"
+  "sockets.exe.manifest"
+  "libsockets.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sockets.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
