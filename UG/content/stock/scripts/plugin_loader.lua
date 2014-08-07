7cc0851211c348c1f075b9d658ce21f5577a052a
InDieTasten <indietasten@gmail.com>
2014-08-07 19:26:29 +0200

    Plugin-Hosting done!

--- UG/content/stock/scripts/plugin_loader.lua
+++ UG/content/stock/scripts/plugin_loader.lua
@@ -1,8 +1,20 @@
+function loadAPI(f,g)
+    _G[g]=setmetatable({},{__index=_G})
+    loadfile(f,"bt",_G[g])()
+end
+
+--loadAPI("example.lua", "externAPI")
+--print(externAPI.apiFunction("Test"))
+
 function onLoad()
 	print("plugin_loader.lua loaded")
 	print("")
 	print("looking for plugins...")
-	for dir in io.popen([[dir "content/plugins" /b]]):lines() do print(dir) end
+	for dir in io.popen([[dir "content/plugins" /b]]):lines() do
+		print(dir)
+	end
+	loadAPI("content/plugins/plugin01.lua", "wurzel")
+	wurzel.troll()
 	print("")
 	print("C Path: ", package.cpath)
 end
