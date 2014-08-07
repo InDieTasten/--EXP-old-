ee2875ec6cda29299162eba21e2f362eb5d284fb
InDieTasten <indietasten@gmail.com>
2014-08-07 03:52:42 +0200

    Plugin-Loader Part 1
    Former-commit-id: bb7a8d1cde63d73c5ecdb9797aab8d42a9bcbfc2 [formerly 82c4593eca4d0c8565c5691232b18eff97bb006d]
    Former-commit-id: 70fc9ada44ce6308e9823b154c42531957adaf40

--- UG/content/stock/scripts/console.lua
+++ UG/content/stock/scripts/console.lua
@@ -14,17 +14,15 @@ local function printTable(table, i)
 end
 
 function onLoad()
-	print("console.lua loaded :)")
-	printTable(_G)
+	print("console.lua loaded")
+	--printTable(_G)
 end
 function onUnload()
-	print("console.lua unloaded :(")
+	print("console.lua unloaded")
 end
 function onSoftEvent(...)
-	print("\\/Soft-Event captured\\/")
 	mytab = {...}
 	for k,v in pairs(mytab) do
 		print(tostring(k)..": "..tostring(v))
 	end
-	print("/\\Soft-Event captured/\\")
 end
