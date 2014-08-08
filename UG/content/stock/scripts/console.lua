b432fffa24f6e362342161cfff2d31694e00e5bd
InDieTasten <indietasten@gmail.com>
2014-08-08 02:51:32 +0200

    Lua-Side plugin host
    Former-commit-id: 0b282e262f8326a5efffa679264ef88acf62b388

--- UG/content/stock/scripts/console.lua
+++ UG/content/stock/scripts/console.lua
@@ -1,24 +1,8 @@
--- this is the [[UG]]-Console Interpreter
-
-local function printTable(table, i)
-	if(i == nil) then i = 0 end
-	if (i > 10) then return end
-	for k,v in pairs(table) do
-		print(string.rep(" ",i*2),type(v), ": " ,k)
-		if(type(v) == "table") then
-			if(k ~= "_G" and k ~= "package") then
-				printTable(v, i+1)
-			end
-		end
-	end
-end
-
 function onLoad()
-	print("console.lua loaded")
-	--printTable(_G)
+
 end
 function onUnload()
-	print("console.lua unloaded")
+
 end
 function onSoftEvent(...)
 	e = {...}
