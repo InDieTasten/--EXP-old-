1d7fc62891f25eeeb722ae93d789154a5e8b9e47
InDieTasten <indietasten@gmail.com>
2014-08-26 00:36:00 +0200

    Global pointer: Step 1
    Former-commit-id: 6e871c2cc703c9a198a105e2415a962f6359b2af [formerly 5253603f110bfe4ef74c400e1fca46072cf25d70]
    Former-commit-id: b000c80495dac7f3ef49098af725d529ebcac474

--- UG/TextBox.hpp
+++ UG/TextBox.hpp
@@ -7,40 +7,40 @@
 
 class TextBox
 {
-	//MEMBERS
+    //MEMBERS
 private:
-	sf::RectangleShape rect;
-	std::string tmp;
-	std::string tmp2;
-	std::string tmp3;
-	bool clear;
-	sf::RectangleShape c;
-	int cursor;
-	int line;
+    sf::RectangleShape rect;
+    std::string tmp;
+    std::string tmp2;
+    std::string tmp3;
+    bool clear;
+    sf::RectangleShape c;
+    int cursor;
+    int line;
 
 public:
-	std::list<std::string> text;
+    std::list<std::string> text;
     std::list<sf::Text> text1;
-	int X;
-	int Y;
-	int Height;
-	int Width;
-	std::string ID;
-	bool isActive;
-	bool multiline;
+    int X;
+    int Y;
+    int Height;
+    int Width;
+    std::string ID;
+    bool isActive;
+    bool multiline;
 
-	//CONSTRUCTORS
+    //CONSTRUCTORS
 
-	//DESTRUCTORS
+    //DESTRUCTORS
 
-	//METHODS
-	void Show();
-	void Hide();
+    //METHODS
+    void Show();
+    void Hide();
 
-	void Setup();
-	void Update(DataBank* _dataLink, int _x, int _y,std::string _id);
-	void Render(DataBank* _dataLink, int _x, int _y, std::string _id);
-	void handleEvent(DataBank* _dataLink,sf::Event* _event, int _x, int _y,std::string _id);
+    void Setup();
+    void Update(DataBank* _dataLink, int _x, int _y,std::string _id);
+    void Render(DataBank* _dataLink, int _x, int _y, std::string _id);
+    void handleEvent(DataBank* _dataLink,sf::Event* _event, int _x, int _y,std::string _id);
 };
 
 #endif
