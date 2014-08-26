c22f7b5b723907d48f4df3861ebbf6c880c81b9d
InDieTasten <indietasten@gmail.com>
2014-08-27 00:33:53 +0200

    Lua: Console Interpreter Done!
    Former-commit-id: a81377853172f6ae447e1acea53b666e38780d1d

--- UG/TextBox.cpp
+++ UG/TextBox.cpp
@@ -26,12 +26,13 @@ void TextBox::Update(int _x, int _y,std::string _id)
     }
     else
     {
-        text.setColor(sf::Color::White);
-        text.setPosition(_x+1, _y+1);
-        text.setFont(*dLink->FontGet("$_menuTitle"));
-        text.setCharacterSize(12);
-        text.setString(content);
+
     }
+    text.setColor(sf::Color::White);
+    text.setPosition(_x+1, _y+1);
+    text.setFont(*dLink->FontGet("$_menuTitle"));
+    text.setCharacterSize(12);
+    text.setString(content);
 }
 void TextBox::Render(int _x, int _y, std::string _id)
 {
@@ -44,6 +45,68 @@ void TextBox::handleEvent(sf::Event* _event, int _x, int _y,std::string _id)
     {
         if(multiline)
         {
+            if(_event->type == sf::Event::KeyPressed)
+            {
+                if(_event->key.code == sf::Keyboard::Left)
+                {
+                    cursorX--;
+                    if(cursorX < 1)
+                        cursorX = 1;
+                }
+                if(_event->key.code == sf::Keyboard::Right)
+                {
+                    cursorX++;
+                    if(cursorX > content.size()+1)
+                        cursorX = content.size()+1;
+                }
+                if(_event->key.code == sf::Keyboard::Up)
+                {
+                    cursorY--;
+                    if(cursorX < 1)
+                        cursorX = 1;
+                }
+                if(_event->key.code == sf::Keyboard::Down)
+                {
+                    cursorY-;
+                    if(cursorX < 1)
+                        cursorX = 1;
+                }
+            }
+            else if(_event->type == sf::Event::TextEntered)
+            {
+                std::cout << _event->text.unicode << std::endl;
+                if (static_cast<char>(_event->text.unicode) == sf::Uint32(13)) {
+                    content += "\n";
+
+                    return;
+                }
+                if (static_cast<char>(_event->text.unicode) == '\b') {
+                    if(content.size() == 0) { return; }
+                    std::string::iterator it = content.begin();
+                    for(int i = 1; i < cursorX-1; i++)
+                    {
+                        it++;
+                    }
+                    content.erase(it);
+                    cursorX--;
+                    return;
+                }
+                if (_event->text.unicode < 128)
+                {
+                    std::string::iterator it = content.begin();
+                    for(int i = 1; i < cursorX; i++)
+                    {
+                        it++;
+                    }
+                    content.insert(it,static_cast<char>(_event->text.unicode));
+                    std::list<std::string> x;
+                    x.push_back("textbox_insert");
+                    x.push_back(_id);
+                    x.push_back(content);
+                    dLink->pushEvent(x);
+                    cursorX++;
+                }
+            }
         }
         else
         {
