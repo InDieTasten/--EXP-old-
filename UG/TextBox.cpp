58d1da8b2f69f99754c8f6c42de3fd2211f4b93c
InDieTasten <indietasten@gmail.com>
2014-08-28 15:18:52 +0200

    MultiLine done
    Multiline funktioniert
    Singleline muss ich neu schreiben
    
    
    Former-commit-id: 15c7b3313d934b0d52d4b823b4d458430cc331e1

--- UG/TextBox.cpp
+++ UG/TextBox.cpp
@@ -5,12 +5,15 @@
 //DESTRUCTORS
 
 //METHODS
+void TextBox::calibrateCursor()
+{
+    cursor = content.begin();
+    for(int i = 0; i < position; i++) {cursor++;}
+}
 void TextBox::Setup()
 {
-    cursorX = 1;
-    cursorY = 1;
+    position = 0;
     content = "";
-    multiline = false;
     isActive = true;
 }
 void TextBox::Update(int _x, int _y,std::string _id)
@@ -20,6 +23,13 @@ void TextBox::Update(int _x, int _y,std::string _id)
     rect.setFillColor(sf::Color(20,20,20,150));
     rect.setOutlineThickness(1.0f);
     rect.setOutlineColor(sf::Color(255,255,255,50));
+
+    sf::Vector2f temp = text.findCharacterPos(position);
+    curs.setPosition(temp);
+    curs.setSize(sf::Vector2f((float)0,(float)12));
+    curs.setFillColor(sf::Color(0,0,0,128));
+    curs.setOutlineThickness(1.0f);
+    curs.setOutlineColor(sf::Color(0,255,0,255));
     if(multiline)
     {
 
@@ -38,6 +48,7 @@ void TextBox::Render(int _x, int _y, std::string _id)
 {
     dLink->renderWindow->draw(rect);
     dLink->renderWindow->draw(text);
+    dLink->renderWindow->draw(curs);
 }
 void TextBox::handleEvent(sf::Event* _event, int _x, int _y,std::string _id)
 {
@@ -49,124 +60,70 @@ void TextBox::handleEvent(sf::Event* _event, int _x, int _y,std::string _id)
             {
                 if(_event->key.code == sf::Keyboard::Left)
                 {
-                    cursorX--;
-                    if(cursorX < 1)
-                        cursorX = 1;
+                    if(position > 0)
+                    {
+                        position--;
+                    }
                 }
                 if(_event->key.code == sf::Keyboard::Right)
                 {
-                    cursorX++;
-                    if(cursorX > content.size()+1)
-                        cursorX = content.size()+1;
+                    if(position < content.size())
+                    {
+                        position++;
+                    }
                 }
-                if(_event->key.code == sf::Keyboard::Up)
+                if(_event->key.code == sf::Keyboard::Delete)
                 {
-                    cursorY--;
-                    if(cursorX < 1)
-                        cursorX = 1;
-                }
-                if(_event->key.code == sf::Keyboard::Down)
-                {
-                    cursorY--;
-                    if(cursorX < 1)
-                        cursorX = 1;
+                    if(position < content.size())
+                    {
+                        position++;
+                        if(position > 0)
+                        {
+                            position--;
+                            calibrateCursor();
+                            content.erase(cursor);
+                        }
+                    }
                 }
             }
             else if(_event->type == sf::Event::TextEntered)
             {
-                std::cout << _event->text.unicode << std::endl;
                 if (static_cast<char>(_event->text.unicode) == sf::Uint32(13)) {
-                    content += "\n";
-
+                    calibrateCursor();
+                    content.insert(cursor,'\n');
+                    position++;
                     return;
                 }
                 if (static_cast<char>(_event->text.unicode) == '\b') {
-                    if(content.size() == 0) { return; }
-                    std::string::iterator it = content.begin();
-                    for(int i = 1; i < cursorX-1; i++)
+                    if(position > 0)
                     {
-                        it++;
+                        position--;
+                        calibrateCursor();
+                        content.erase(cursor);
                     }
-                    content.erase(it);
-                    cursorX--;
                     return;
                 }
                 if (_event->text.unicode < 128)
                 {
-                    std::string::iterator it = content.begin();
-                    for(int i = 1; i < cursorX; i++)
+                    calibrateCursor();
+                    if(content.size() == 0)
                     {
-                        it++;
+                        content.push_back(static_cast<char>(_event->text.unicode));
+                        cursor = content.begin();
+                    } else {
+                    content.insert(cursor,static_cast<char>(_event->text.unicode));
                     }
-                    content.insert(it,static_cast<char>(_event->text.unicode));
-                    std::list<std::string> x;
+                    /*std::list<std::string> x;
                     x.push_back("textbox_insert");
                     x.push_back(_id);
                     x.push_back(content);
-                    dLink->pushEvent(x);
-                    cursorX++;
+                    dLink->pushEvent(x);*/
+                    position++;
                 }
             }
         }
         else
         {
-            if(_event->type == sf::Event::KeyPressed)
-            {
-                if(_event->key.code == sf::Keyboard::Return)
-                {
-
-                }
-                if(_event->key.code == sf::Keyboard::Left)
-                {
-                    cursorX--;
-                    if(cursorX < 1)
-                        cursorX = 1;
-                }
-                if(_event->key.code == sf::Keyboard::Right)
-                {
-                    cursorX++;
-                    if(cursorX > content.size()+1)
-                        cursorX = content.size()+1;
-                }
-            }
-            else if(_event->type == sf::Event::TextEntered)
-            {
-                std::cout << _event->text.unicode << std::endl;
-                if (static_cast<char>(_event->text.unicode) == sf::Uint32(13)) {
-                    std::list<std::string> x;
-                    x.push_back("textbox_submit");
-                    x.push_back(_id);
-                    x.push_back(content);
-                    dLink->pushEvent(x);
-                    return;
-                }
-                if (static_cast<char>(_event->text.unicode) == '\b') {
-                    if(content.size() == 0) { return; }
-                    std::string::iterator it = content.begin();
-                    for(int i = 1; i < cursorX-1; i++)
-                    {
-                        it++;
-                    }
-                    content.erase(it);
-                    cursorX--;
-                    return;
-                }
-                if (_event->text.unicode < 128)
-                {
-                    std::string::iterator it = content.begin();
-                    for(int i = 1; i < cursorX; i++)
-                    {
-                        it++;
-                    }
-                    content.insert(it,static_cast<char>(_event->text.unicode));
-                    std::list<std::string> x;
-                    x.push_back("textbox_insert");
-                    x.push_back(_id);
-                    x.push_back(content);
-                    dLink->pushEvent(x);
-                    cursorX++;
-                }
-            }
         }
     }
 }
@@ -176,14 +133,15 @@ void TextBox::handleSoftEvent(std::list<std::string> _args, int _x, int _y, std:
 }
 void TextBox::handleTask(std::list<std::string> _args, int _x, int _y, std::string _id)
 {
-    if(*_args.begin() == "textbox_clear")
+    if(!multiline)
     {
-        _args.pop_front();
-        if(*_args.begin() == _id)
+        if(*_args.begin() == "textbox_clear")
         {
-            content = "";
-            cursorX = 1;
-            cursorY = 1;
+            _args.pop_front();
+            if(*_args.begin() == _id)
+            {
+                content = "";
+            }
         }
     }
 }
