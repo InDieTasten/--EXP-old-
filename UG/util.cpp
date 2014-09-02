e61ed27e6ed718f29d21fecef128d8b15a8fcad2
InDieTasten <indietasten@gmail.com>
2014-09-02 17:07:32 +0200

    Utils Fix

--- UG/util.cpp
+++ UG/util.cpp
@@ -37,65 +37,25 @@ char util::getCharAt(std::string _string, int _pos)
 
 int    util::toInt(std::string _value)
 {
-    int result = 0;
-    int base = 10;
-    if(_value.c_str()[0] == '-')
-    {
-        base*=-1;
-        ///_value = subString(_value, 2, _value.length());
-    }
-    for(int x = _value.length(); x > 0; x--)
-    {
-
-        switch(_value.c_str()[x-1])
-        {
-        case '0':
-            result+= 0*base*x;
-            break;
-        case '1':
-            result+= 1*base*x;
-            break;
-        case '2':
-            result+= 2*base*x;
-            break;
-        case '3':
-            result+= 3*base*x;
-            break;
-        case '4':
-            result+= 4*base*x;
-            break;
-        case '5':
-            result+= 5*base*x;
-            break;
-        case '6':
-            result+= 6*base*x;
-            break;
-        case '7':
-            result+= 7*base*x;
-            break;
-        case '8':
-            result+= 8*base*x;
-            break;
-        case '9':
-            result+= 9*base*x;
-            break;
-        default:
-            return -1;
-        }
-        return result;
-    }
+    int result;
+    std::stringstream(_value) >> result;
+    return result;
 }
 bool   util::toBool(std::string _value)
 {
-    return false;
+    return (_value == "true");
 }
 float  util::toFloat(std::string _value)
 {
-    return -1.0;
+    float result;
+    std::stringstream(_value) >> result;
+    return result;
 }
 double util::toDouble(std::string _value)
 {
-    return -1.0;
+    double result;
+    std::stringstream(_value) >> result;
+    return result;
 }
 
 std::list<std::string> util::getFolderContent(std::string _path)
