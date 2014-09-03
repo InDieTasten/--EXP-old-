bb9ee144527084e6ee5f39b583dfe795e8fcca64
InDieTasten <indietasten@gmail.com>
2014-09-03 14:32:21 +0200

    Example plugin

--- UG/content/plugins/plugin01.lua
+++ UG/content/plugins/plugin01.lua
@@ -1,7 +1,23 @@
-local function x()
-	print("XXX")
+function onLoad() -- called right after the plugin has been loaded
+	print("MEEEH was loaded :3")
 end
-function onEvent(...)
-	print("GOTCHA!")
+function onUnload() -- called right before your plugin gets overriden with nil
+	print("noooooh :(")
 end
-print("plugin01.lua loaded")
+
+function onEvent(...) -- gets called when something happens
+	e = {...} -- contains the event parts
+	if(e[1] == "Hello") then
+		if(e[2] == "World") then
+			-- more on these can be read in our documentation
+		end
+	end
+end
+function onTask(...) -- gets called when something should happen
+	t = {...}
+	if(t[1] == "Hello") then
+		if(t[2] == "World") then
+			-- more on these can be read in our documentation
+		end
+	end
+end
