2fd9990445ad0ee4052270e50edaa767d98b0383
InDieTasten <indietasten@gmail.com>
2014-09-03 14:08:53 +0200

    plugin calling now working

--- UG/content/stock/scripts/plugin_loader.lua
+++ UG/content/stock/scripts/plugin_loader.lua
@@ -8,7 +8,7 @@ io = nil
 
 local plugins = {}
 
-function registerPlugin(path, name)
+local function registerPlugin(path, name)
 	if(type(name) ~= "string") then
 		print("[Error] Plugin to be registered must name a string!")
 		return
@@ -27,110 +27,79 @@ function registerPlugin(path, name)
 	plugins[name] = {["path"]=path, ["status"]="unloaded"}
 	print("[Info] Plugin '"..name.."' successfully registered")
 end
-function loadPlugin(name)
-	if(type(name) ~= "string") then
-		print("[Error] Plugin to be loaded must name a string!")
-		return
-	end
-	if(plugins[name]) then
-		if(plugins[name].status == "unloaded") then
-			_G[name]=setmetatable({},{__index=_G})
-		    local pl, err = loadfile(plugins[name].path,"bt",_G[name])
-		    if(pl) then
-		    	pl()
-		    	plugins[name].status = "loaded"
-		    	if(type(_G[name] == "table")) then
-					if(type(_G[name]["onLoad"]) == "function") then
-						status, err = pcall(_G[name]["onLoad"], ...)
-						if(not status) then
-							print("[Error] "..tostring(err))
-						end
-					else
-						print("[Warning] Plugin '"..name.."': onLoad should be a function")
-					end
-				end
-		    	print("[Info] Plugin '"..name.."' successfully loaded")
-		    else
-		    	print("[Error] ", tostring(err))
-		    end
-		else
-			print("[Error] Plugin '"..name.."' already loaded!")
-		end
-	else
-		print("[Error] Plugin '"..name.."' not found!")
-	end
-end
-function unloadPlugin(name)
-	if(type(name) ~= "string") then
-		print("[Error] Plugin to be unloaded must name a string!")
-		return
-	end
-	if(plugins[name]) then
-		if(plugins[name].status == "loaded") then
-			if(type(_G[name] == "table")) then
-				if(type(_G[name]["onUnload"]) == "function") then
-					status, err = pcall(_G[name]["onUnload"], ...)
-					if(not status) then
-						print("[Error] "..tostring(err))
-					end
-				else
-					print("[Warning] Plugin '"..name.."': onUnload should be a function")
-				end
-			end
-			_G[name] = nil
-			plugins[name].status = "unloaded"
-			print("[Info] Plugin '"..name.."' unloaded")
-		else
-			print("[Error] Plugin '"..name.."' already unloaded!")
-		end
-	else
-		print("[Error] Plugin '"..name.."' not found!")
-	end
-end
-function reloadPlugin(name)
-	unloadPlugin(name)
-	loadPlugin(name)
-end
-function checkPlugin(name)
-	--
-	--
-end
+--loading
+--_G[name]=setmetatable({},{__index=_G})
+--local pl, err = loadfile(plugins[name].path,"bt",_G[name])
+--if(pl) then st, err = pcall(pl) else print(err) end
 
---loadAPI("example.lua", "externAPI")
---print(externAPI.apiFunction("Test"))
 
-function onLoad()
+function onHardLoad()
 	for dir in popen([[dir "content/plugins" /b]]):lines() do
 		registerPlugin("content/plugins/"..dir, string.sub(dir, 1, #dir-4))
 	end
 end
-function onUnload()
+function onHardUnload()
 	print("plugin_loader.lua unloaded")
 end
-function onSoftEvent(...)
-	for name,plugin in pairs(plugins) do
-		if (plugin.status == "loaded") then
-			if(type(_G[name] == "table")) then
-				if(type(_G[name]["onSoftEvent"]) == "function") then
-					status, err = pcall(_G[name]["onSoftEvent"], ...)
-					if(not status) then
-						print("[Error] "..tostring(err))
-					end
-				else
-					print("[Warning] Plugin '"..name.."': onSoftEvent should be a function")
-				end
+function onHardEvent(...)
+	for name, detail in pairs(plugins) do
+		if(type(_G[name]) == "table") then
+			if(type(_G[ name ][ "onEvent" ])=="function") then
+				st, err = pcall(_G[ name ][ "onEvent" ], ...)
+				if(not st) then print(err) end
 			end
 		end
 	end
 end
-function onTask(...)
-	
+function onHardTask(...)
+	cmd = {...}
+	if(cmd[1] == "plugin") then
+		if(cmd[2] == "call") then
+			st, err = pcall(_G[ cmd[3] ][ cmd[4] ])
+			if(not st) then print(err) end
+		elseif(cmd[2] == "unload") then
+			if(type(_G[ cmd[3] ]) == "table") then
+				if(type(_G[ cmd[3] ][ "onUnload" ])=="function") then
+					st, err = pcall(_G[ cmd[3] ][ "onUnload" ], ...)
+					if(not st) then print(err) end
+				end
+			end
+			_G[ cmd[3] ] = nil
+		elseif(cmd[2] == "load") then
+			_G[ cmd[3] ] = setmetatable({},{__index=_G})
+			local pl, err = loadfile(plugins[ cmd[3] ].path,"bt",_G[ cmd[3] ])
+			if(pl) then st, err = pcall(pl) if(not st) then print(err) end else print(err) end
+			if(type(_G[ cmd[3] ]) == "table") then
+				if(type(_G[ cmd[3] ][ "onLoad" ])=="function") then
+					st, err = pcall(_G[ cmd[3] ][ "onLoad" ], ...)
+					if(not st) then print(err) end
+				end
+			end
+		elseif(cmd[2] == "reload") then
+			if(type(_G[ cmd[3] ]) == "table") then
+				if(type(_G[ cmd[3] ][ "onUnload" ])=="function") then
+					st, err = pcall(_G[ cmd[3] ][ "onUnload" ], ...)
+					if(not st) then print(err) end
+				end
+			end
+			_G[ cmd[3] ] = nil
+			_G[ cmd[3] ] = setmetatable({},{__index=_G})
+			local pl, err = loadfile(plugins[ cmd[3] ].path,"bt",_G[ cmd[3] ])
+			if(pl) then st, err = pcall(pl) if(not st) then print(err) end else print(err) end
+			if(type(_G[ cmd[3] ]) == "table") then
+				if(type(_G[ cmd[3] ][ "onLoad" ])=="function") then
+					st, err = pcall(_G[ cmd[3] ][ "onLoad" ], ...)
+					if(not st) then print(err) end
+				end
+			end
+		end
+	end
+	for name, detail in pairs(plugins) do
+		if(type(_G[name]) == "table") then
+			if(type(_G[ name ][ "onTask" ])=="function") then
+				st, err = pcall(_G[ name ][ "onTask" ], ...)
+				if(not st) then print(err) end
+			end
+		end
+	end
 end
-
---local coroutine = coroutine
---local debug = debug
---local load = load
---local require = require
---local io = io
---local package = package
---local collectgarbage = collectgarbage
