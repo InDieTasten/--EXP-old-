5cd6cf3110dd44c083b264db3dd0ec132e786a47
MouseScroller <mousescroller@gmail.com>
2014-09-03 23:04:22 +0200

    Logger fix 2
    Former-commit-id: f518f18660e94a4de311215d85e0c8037303295d [formerly fdd21d1782b0a283643a25a91d23fe60aa313714]
    Former-commit-id: d7a785eed2e04b6cc7ddf1a181631889331269d2

--- UG/Logger.cpp
+++ UG/Logger.cpp
@@ -7,15 +7,15 @@ void log(std::string name, std::string msg)
     time_t rawtime;
     struct tm * timeinfo;
 
-    char T[10]; //Stunden
+    char T[10]; //HH,mm,ss
     char D[10]; //Tag
 
     time (&rawtime);
     timeinfo = localtime (&rawtime);
 
-    strftime (T,20,"%T",timeinfo);
+    strftime (T,20,"%H:%M:%S",timeinfo);
     strftime (D,20,"%j",timeinfo);
 
 
-    std::cout << T << "|" << D << "[" << name << "] " << msg << std::endl;
+    std::cout << D << "|" << T << "[" << name << "] " << msg << std::endl;
 }
