3e492404f4744e9cd9bcdbf09f0e678fa27349f3
InDieTasten <indietasten@gmail.com>
2014-09-03 22:52:05 +0200

    Logger Fix
    Former-commit-id: 502c890d2ecf7e41958af36c9e1256728e498e82 [formerly 1a3566123fc2b0006ad853169c691c8db39d371d]
    Former-commit-id: 06f594a6347f607f670dd0ab43783483c07b5acc

--- UG/Logger.cpp
+++ UG/Logger.cpp
@@ -4,6 +4,12 @@
 //METHODS
 void log(std::string name, std::string msg)
 {
+    time_t rawtime;
+    struct tm * timeinfo;
+
+    char T[10]; //Stunden
+    char D[10]; //Tag
+
     time (&rawtime);
     timeinfo = localtime (&rawtime);
 
