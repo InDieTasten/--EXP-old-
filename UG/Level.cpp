8c4a89f972140e0b05cb77e53d93dfb15a0f086a
InDieTasten <indietasten@gmail.com>
2014-09-08 18:23:10 +0200

    Level task interpretation
    Former-commit-id: 07cddf24e5f3c8521c75db910bdc9bd5b7e44bf1 [formerly aa83de8dd9d6dd223b0921f209ee3a165cc985f6]
    Former-commit-id: fee740787b3b479476b50bd5d76bb9fa7dcf0001

--- UG/Level.cpp
+++ UG/Level.cpp
@@ -11,5 +11,9 @@ void Level::handleSoftEvent(std::list<std::string> _args)
 }
 void Level::handleTask(std::list<std::string> _args)
 {
-
+    if(*_args.begin() == "active")
+    {
+        _args.pop_front();
+        activeSystem.handleTask(_args);
+    }
 }
