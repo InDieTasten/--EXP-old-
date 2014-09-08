aa83de8dd9d6dd223b0921f209ee3a165cc985f6
InDieTasten <indietasten@gmail.com>
2014-09-08 18:23:10 +0200

    Level task interpretation

--- UG/SolarSystem.cpp
+++ UG/SolarSystem.cpp
@@ -4,3 +4,40 @@
 //DESTRUCTORS
 
 //METHODS
+void SolarSystem::handleSoftEvent(std::list<std::string> _args)
+{
+
+}
+void SolarSystem::handleTask(std::list<std::string> _args)
+{
+    if(*_args.begin() == "addObject")
+    {
+        _args.pop_front();
+        SpaceObject tmp;
+        tmp.ID = *_args.begin();
+        SpaceObjectList.push_back(tmp);
+    } else if (*_args.begin() == "removeObject")
+    {
+        _args.pop_front();
+        for(std::list<SpaceObject>::iterator it = SpaceObjectList.begin(); it != SpaceObjectList.end(); it++)
+        {
+            if(it->ID == *_args.begin())
+            {
+                SpaceObjectList.erase(it);
+                break;
+            }
+        }
+    } else if (*_args.begin() == "modifyObject")
+    {
+        _args.pop_front();
+        for(std::list<SpaceObject>::iterator it = SpaceObjectList.begin(); it != SpaceObjectList.end(); it++)
+        {
+            if(it->ID == *_args.begin())
+            {
+                _args.pop_front();
+                it->handleTask(_args);
+                break;
+            }
+        }
+    }
+}
