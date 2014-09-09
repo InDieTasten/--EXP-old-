3bca0cea2c6c5b1204cb709f14ed3345e4910b55
InDieTasten <indietasten@gmail.com>
2014-09-09 09:20:02 +0200

    PhysicsEngine rewrite
    Former-commit-id: 684b61804caaea1f2ab92d63f2659b02b955a611 [formerly 6285855d3285baab2c9066b217af448770146e65]
    Former-commit-id: 1d152a864e1d0db1c65fad199d4a5b117454c05b

--- UG/PhysicsEngine.cpp
+++ UG/PhysicsEngine.cpp
@@ -10,35 +10,7 @@ PhysicsEngine::PhysicsEngine()
 //DESTRUCTORS
 
 //METHODS
-double PhysicsEngine::calcDistanceX(Vector _obj1, Vector _obj2)
-{
-    double distX;
-    if(_obj1.x > _obj2.x)
-        distX = _obj1.x - _obj2.x;
-    else if (_obj1.x < _obj2.x)
-        distX = _obj2.x - _obj1.x;
-    else
-        distX = 0.0f;
-    return distX;
-}
-double PhysicsEngine::calcDistanceY(Vector _obj1, Vector _obj2)
-{
-    double distY;
-    if(_obj1.y > _obj2.y)
-        distY = _obj1.y - _obj2.y;
-    else if (_obj1.y < _obj2.y)
-        distY = _obj2.y - _obj1.y;
-    else
-        distY = 0.0f;
-    return distY;
-}
-float PhysicsEngine::calcDistance(Vector _obj1, Vector _obj2)
-{
-    double distX = calcDistanceX(_obj1, _obj2);
-    double distY = calcDistanceY(_obj1, _obj2);
-    float dist = sqrtf((float)distX*(float)distX+(float)distY*(float)distY);
-    return dist;
-}
+
 void PhysicsEngine::Run()
 {
     log("P-Engine  ", "Thread launched");
@@ -53,42 +25,15 @@ void PhysicsEngine::Run()
         }
         limit.restart();
         GMutex.lock();
-        for (std::list<SpaceObject>::iterator sObject = dLink->level.activeSystem.SpaceObjectList.begin(); sObject != dLink->level.activeSystem.SpaceObjectList.end(); sObject++)
-        {
 
-            ////PlayerInput
-            //instant rotation
-            //sObject->Rotation = sObject->controller.targetRotation;
+        //Set active actor to input when not in guiMode
 
-            //translation
+        //
 
+        //
 
-            ////GRAVITY
-            for (std::list<SpaceObject>::iterator sObject2 = dLink->level.activeSystem.SpaceObjectList.begin(); sObject2 != dLink->level.activeSystem.SpaceObjectList.end(); sObject2++)
-            {
-                //calc gravity force
-                if (sObject->ID != sObject2->ID)
-                {
-                    double m1 = sObject->Mass;
-                    double m2 = sObject2->Mass;
-                    double r = calcDistance(sObject->Position, sObject2->Position);
-                    if (r == 0)
-                        break;
+        //
 
-                    double gForce = G*m1*m2/r/r;
-
-                    //rational distance between the objects
-                    Vector Distance = Vector(sObject2->Position.x - sObject->Position.x,sObject2->Position.y - sObject->Position.y);
-
-                    Vector Force = Vector(Distance.x/r*gForce,Distance.y/r*gForce);
-
-                    //add change of velocity influenced by the mass(inertia) and frametime
-                    sObject->Velocity = Vector( sObject->Velocity.x + Force.x/sObject->Mass*frametime, sObject->Velocity.y + Force.y/sObject->Mass*frametime );
-                }
-            }
-            //new position
-            sObject->Position = Vector(sObject->Position.x + sObject->Velocity.x*frametime, sObject->Position.y + sObject->Velocity.y*frametime);
-        }
         GMutex.unlock();
     }
     log("P-Engine  ", "Thread stopped");
