c6cf2a615a20afea99f91c7db3af7df2e68b373c
InDieTasten <indietasten@gmail.com>
2014-09-14 17:06:23 +0200

    Level task interpret. kill
    Former-commit-id: 03558c35601f9bd2e7eabf50a6e08c2418e74238 [formerly e5be77751fcf39efefb758c361f18a9f704015b0]
    Former-commit-id: be799eee8442765c291c7c8d66e43ee7b965f0f2

--- UG/SolarSystem.cpp
+++ UG/SolarSystem.cpp
@@ -10,49 +10,5 @@ void SolarSystem::handleSoftEvent(std::list<std::string> _args)
 }
 void SolarSystem::handleTask(std::list<std::string> _args)
 {
-    if(*_args.begin() == "addObject")
-    {
-        _args.pop_front();
-        SpaceObject tmp;
-        tmp.ID = *_args.begin();
-        SpaceObjectList.push_back(tmp);
-    }
-    else if (*_args.begin() == "removeObject")
-    {
-        _args.pop_front();
-        for(std::list<SpaceObject>::iterator it = SpaceObjectList.begin(); it != SpaceObjectList.end(); it++)
-        {
-            if(it->ID == *_args.begin())
-            {
-                SpaceObjectList.erase(it);
-                break;
-            }
-        }
-    }
-    else if (*_args.begin() == "modifyObject")
-    {
-        std::list<std::string> tmp = _args;
-        _args.pop_front();
-        for(std::list<SpaceObject>::iterator it = SpaceObjectList.begin(); it != SpaceObjectList.end(); it++)
-        {
-            if(it->ID == *_args.begin())
-            {
-                it->handleTask(tmp);
-                break;
-            }
-        }
-    }
-    else if (*_args.begin() == "getObjectDef")
-    {
-        std::list<std::string> tmp = _args;
-        _args.pop_front();
-        for(std::list<SpaceObject>::iterator it = SpaceObjectList.begin(); it != SpaceObjectList.end(); it++)
-        {
-            if(it->ID == *_args.begin())
-            {
-                it->handleTask(tmp);
-                break;
-            }
-        }
-    }
+
 }
