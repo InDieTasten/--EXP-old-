e5be77751fcf39efefb758c361f18a9f704015b0
InDieTasten <indietasten@gmail.com>
2014-09-14 17:06:23 +0200

    Level task interpret. kill

--- UG/Level.cpp
+++ UG/Level.cpp
@@ -11,9 +11,5 @@ void Level::handleSoftEvent(std::list<std::string> _args)
 }
 void Level::handleTask(std::list<std::string> _args)
 {
-    if(*_args.begin() == "active")
-    {
-        _args.pop_front();
-        activeSystem.handleTask(_args);
-    }
+
 }
