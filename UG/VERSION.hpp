54155004b98bb4da44c236463d2e8f3b09bb0aca
InDieTasten <indietasten@gmail.com>
2014-09-20 17:11:52 +0200

    version information

---
+++ UG/VERSION.hpp
@@ -0,0 +1,13 @@
+#ifndef _VERSION_hpp_
+#define _VERSION_hpp_
+
+#include "string"
+
+namespace VERSION
+{
+    std::string name = "[[Untitled Game]]";
+    std::string version = "v0.9.2-pre-alpha.1";
+}
+
+#endif // _VERSION_hpp_
+
