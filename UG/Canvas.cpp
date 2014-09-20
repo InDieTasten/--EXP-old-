58a1352b1369a1567f5e541b80fbb4282970a09b
InDieTasten <indietasten@gmail.com>
2014-09-20 09:34:02 +0200

    Canvas successful tests !
    Former-commit-id: d14c7c140bc4a7c5989af00b8e3c4f65c8733653 [formerly 7601b3920abc9ad2a1b9850dc8436f7b7a2a5d71]
    Former-commit-id: 5e1474e7d9134bbdad50703d403a9b5d021193f3

--- UG/Canvas.cpp
+++ UG/Canvas.cpp
@@ -14,15 +14,17 @@ void Canvas::Update(int _x, int _y, std::string _id)
 {
     rect.setPosition((float)_x,(float)_y);
     rect.setSize(sf::Vector2f((float)Width,(float)Height));
-    rect.setFillColor(sf::Color(0,0,255,255));
+    rect.setFillColor(sf::Color(0,0,0,0));
     rect.setOutlineThickness(1.0f);
     rect.setOutlineColor(sf::Color(255,0,0,255));
 }
 void Canvas::Render(int _x, int _y, std::string _id)
 {
     dLink->renderWindow->draw(rect);
-    for(std::list<line>::iterator it = vertex.begin(); it != vertex.end(); it++)
+    std::list<line> temp = vertex;
+    for(std::list<line>::iterator it = temp.begin(); it != temp.end(); it++)
     {
+
         dLink->renderWindow->draw(it->line, 2, sf::Lines);
     }
 }
