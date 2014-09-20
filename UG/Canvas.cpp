e0fce9752d655dfdce00b333037183900473cf38
InDieTasten <indietasten@gmail.com>
2014-09-20 07:15:39 +0200

    Canvas preparations 3
    Former-commit-id: 0d13c7d3a6fb819ed45069af1ce24a6a3c09e3e9 [formerly f70517e775f3a4732d2276eb3563c2780a461134]
    Former-commit-id: 8e28fb3145392cfad7116402dca44374000f3a3b

---
+++ UG/Canvas.cpp
@@ -0,0 +1,32 @@
+
+#include "Canvas.hpp"
+
+//CONSTRUCTORS
+
+//DESTRUCTORS
+
+//METHODS
+void Canvas::Setup()
+{
+
+}
+void Canvas::Update(int _x, int _y, std::string _id)
+{
+
+}
+void Canvas::Render(int _x, int _y, std::string _id)
+{
+
+}
+void Canvas::handleEvent(sf::Event* _event, int _x, int _y, std::string _id)
+{
+
+}
+void Canvas::handleSoftEvent(std::list<std::string> _args, int _x, int _y, std::string _id)
+{
+
+}
+void Canvas::handleTask(std::list<std::string> _args, int _x, int _y, std::string _id)
+{
+
+}
