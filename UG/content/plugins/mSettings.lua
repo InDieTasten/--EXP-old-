274b543b8ffc3d3f854e50c41e77c07a0db1c0e6
InDieTasten <indietasten@gmail.com>
2014-09-21 12:24:39 +0200

    Adding settings menu part 1
    Former-commit-id: 2c6ab14cb0fe64610173b3a0b16b0b8bec35f442

--- UG/content/plugins/mSettings.lua
+++ UG/content/plugins/mSettings.lua
@@ -1,24 +1,43 @@
+local menu = ""
+local function genMenu()
+	menu = "settings"
+	pushTask("gui","menu","add", menu)
+	pushTask("gui","menu", "access", menu, "property", "x", "set", "50")
+	pushTask("gui","menu", "access", menu, "property", "y", "set", "50")
+	pushTask("gui","menu", "access", menu, "property", "width", "set", "300")
+	pushTask("gui","menu", "access", menu, "property", "height", "set", "400")
+	pushTask("gui","menu", "access", menu, "property", "widthT", "set", "300")
+	pushTask("gui","menu", "access", menu, "property", "heightT", "set", "400")
+	pushTask("gui","menu", "access", menu, "property", "visible", "set", "true")
+	pushTask("gui","menu", "access", menu, "property", "scrollable", "set", "false")
+	pushTask("gui","menu", "access", menu, "property", "title", "set", "--[[ SETTINGS ]]--")
+end
+local function label(id,y,text)
+	menu = "settings"
+	pushTask("gui","menu", "access", menu, "element", "add", id)
+	pushTask("gui","menu", "access", menu, "element", "access", id, "property", "type", "set", "label")
+	pushTask("gui","menu", "access", menu, "element", "access", id, "property", "x", "set", "10")
+	pushTask("gui","menu", "access", menu, "element", "access", id, "property", "y", "set", tostring(y))
+	pushTask("gui","menu", "access", menu, "element", "access", id, "property", "displayText", "set", tostring(text))
+end
+local function slider(id,y,min,max,val)
+	menu = "settings"
+	pushTask("gui","menu", "access", menu, "element", "add", id)
+	pushTask("gui","menu", "access", menu, "element", "access", id, "property", "type", "set", "horSlider")
+	pushTask("gui","menu", "access", menu, "element", "access", id, "property", "x", "set", "150")
+	pushTask("gui","menu", "access", menu, "element", "access", id, "property", "y", "set", tostring(y))
+	pushTask("gui","menu", "access", menu, "element", "access", id, "property", "width", "set", "130")
+	pushTask("gui","menu", "access", menu, "element", "access", id, "property", "height", "set", "16")
+	pushTask("gui","menu", "access", menu, "element", "access", id, "property", "min", "set", tostring(min))
+	pushTask("gui","menu", "access", menu, "element", "access", id, "property", "max", "set", tostring(max))
+	pushTask("gui","menu", "access", menu, "element", "access", id, "property", "val", "set", tostring(val))
+end
 function onLoad() -- called right after the plugin has been loaded
-	pushTask("gui","menu","add","can")
-	pushTask("gui","menu", "access", "can", "property", "x", "set", "50")
-	pushTask("gui","menu", "access", "can", "property", "y", "set", "50")
-	pushTask("gui","menu", "access", "can", "property", "width", "set", "200")
-	pushTask("gui","menu", "access", "can", "property", "height", "set", "200")
-	pushTask("gui","menu", "access", "can", "property", "visible", "set", "true")
-	pushTask("gui","menu", "access", "can", "property", "title", "set", ".canvas-.test-")
-	pushTask("gui","menu", "access", "can", "element", "add", "canvas")
-	pushTask("gui","menu", "access", "can", "element", "access", "canvas", "property", "type", "set", "canvas")
-	pushTask("gui","menu", "access", "can", "element", "access", "canvas", "property", "x", "set", "10")
-	pushTask("gui","menu", "access", "can", "element", "access", "canvas", "property", "y", "set", "10")
-	pushTask("gui","menu", "access", "can", "element", "access", "canvas", "property", "width", "set", "180")
-	pushTask("gui","menu", "access", "can", "element", "access", "canvas", "property", "height", "set", "180")
-	pushTask("gui","menu", "access", "can", "element", "access", "canvas", "action", "setFillColor", "#0000FFFF")
-	pushTask("gui","menu", "access", "can", "element", "access", "canvas", "action", "draw", "line", "0","0","180","180")
-	pushTask("gui","menu", "access", "can", "element", "access", "canvas", "action", "draw", "line", "0","180","180","0")
-	pushTask("gui","menu", "access", "can", "element", "access", "canvas", "action", "display")
+	genMenu()
+	label("label1", 1*18, "Dock width:") slider("dockWidth", 1*18, 10, 200, 64)
 end
 function onUnload() -- called right before your plugin gets overriden with nil
-	pushTask("gui","menu","remove","can")
+	pushTask("gui","menu","remove", "settings")
 end
 
 function onEvent(...) -- gets called when something happens
