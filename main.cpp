800ed520e2ed30332308b3841fcb92ecfe4a405d
bubbley <werremeyerjan@yahoo.de>
2014-09-22 21:35:18 +0200

    Spell checking
    Former-commit-id: f7c3cf721926a2619991fb401506a13fd847c10e

--- main.cpp
+++ main.cpp
@@ -194,7 +194,7 @@ int main ( int argc, char *argv[] )
     }
     GMutex.unlock();
 
-    printf("\n=|=|=|=|=|=|=|=|=|=|=|=|=|=\n\nThe game closed. If you see errors above, pls contact the developers");
+    printf("\n=|=|=|=|=|=|=|=|=|=|=|=|=|=\n\nThe game closed. If you see errors above, please contact the developers");
     //getchar();
     return EXIT_SUCCESS;
 }
