c7adffff2b0d42664a268eda0ed0e338894c9bf2
InDieTasten <indietasten@gmail.com>
2014-09-24 20:22:35 +0200

    Enable elements to read menuID

--- GUIElement.cpp
+++ GUIElement.cpp
@@ -22,165 +22,165 @@ void GUIElement::Hide()
     isHidden = true;
 }
 
-void GUIElement::update(int _x, int _y)
+void GUIElement::update(int _x, int _y, std::string _mID)
 {
     if(Type == "codebox")
     {
         codeBox.isActive = isActive;
-        codeBox.Update(_x+X, _y+Y, ID);
+        codeBox.Update(_x+X, _y+Y, ID, _mID);
     }
     else if (Type == "button")
     {
         button.isActive = isActive;
-        button.Update(_x+X, _y+Y, ID);
+        button.Update(_x+X, _y+Y, ID, _mID);
     }
     else if (Type == "label")
     {
         label.isActive = isActive;
-        label.Update(_x+X, _y+Y, ID);
+        label.Update(_x+X, _y+Y, ID, _mID);
     }
     else if (Type == "horslider")
     {
         horSlider.isActive = isActive;
-        horSlider.Update(_x+X, _y+Y, ID);
+        horSlider.Update(_x+X, _y+Y, ID, _mID);
     }
     else if (Type == "vertslider")
     {
         vertSlider.isActive = isActive;
-        vertSlider.Update(_x+X, _y+Y, ID);
+        vertSlider.Update(_x+X, _y+Y, ID, _mID);
     }
     else if (Type == "textbox")
     {
         textBox.isActive = true;
-        textBox.Update(_x+X, _y+Y, ID);
+        textBox.Update(_x+X, _y+Y, ID, _mID);
     }
     else if (Type == "canvas")
     {
         canvas.isActive = true;
-        canvas.Update(_x+X, _y+Y, ID);
+        canvas.Update(_x+X, _y+Y, ID, _mID);
     }
 }
-void GUIElement::render(int _x, int _y)
+void GUIElement::render(int _x, int _y, std::string _mID)
 {
     if(Type == "codebox")
     {
-        codeBox.Render(_x+X, _y+Y, ID);
+        codeBox.Render(_x+X, _y+Y, ID, _mID);
     }
     else if (Type == "button")
     {
-        button.Render(_x+X, _y+Y, ID);
+        button.Render(_x+X, _y+Y, ID, _mID);
     }
     else if (Type == "label")
     {
-        label.Render(_x+X, _y+Y, ID);
+        label.Render(_x+X, _y+Y, ID, _mID);
     }
     else if (Type == "horslider")
     {
-        horSlider.Render(_x+X, _y+Y, ID);
+        horSlider.Render(_x+X, _y+Y, ID, _mID);
     }
     else if (Type == "vertslider")
     {
-        vertSlider.Render(_x+X, _y+Y, ID);
+        vertSlider.Render(_x+X, _y+Y, ID, _mID);
     }
     else if (Type == "textbox")
     {
-        textBox.Render(_x+X, _y+Y, ID);
+        textBox.Render(_x+X, _y+Y, ID, _mID);
     }
     else if (Type == "canvas")
     {
-        canvas.Render(_x+X, _y+Y, ID);
+        canvas.Render(_x+X, _y+Y, ID, _mID);
     }
 }
-void GUIElement::handleEvent(sf::Event* _event, int _x, int _y)
+void GUIElement::handleEvent(sf::Event* _event, int _x, int _y, std::string _mID)
 {
     if(Type == "codebox")
     {
-        codeBox.handleEvent(_event, _x+X, _y+Y, ID);
+        codeBox.handleEvent(_event, _x+X, _y+Y, ID, _mID);
     }
     else if (Type == "button")
     {
-        button.handleEvent(_event, _x+X, _y+Y, ID);
+        button.handleEvent(_event, _x+X, _y+Y, ID, _mID);
     }
     else if (Type == "horslider")
     {
-        horSlider.handleEvent(_event, _x+X, _y+Y, ID);
+        horSlider.handleEvent(_event, _x+X, _y+Y, ID, _mID);
     }
     else if (Type == "vertslider")
     {
-        vertSlider.handleEvent(_event, _x+X, _y+Y, ID);
+        vertSlider.handleEvent(_event, _x+X, _y+Y, ID, _mID);
     }
     else if (Type == "label")
     {
-        label.handleEvent(_event, _x+X, _y+Y, ID);
+        label.handleEvent(_event, _x+X, _y+Y, ID, _mID);
     }
     else if (Type == "textbox")
     {
-        textBox.handleEvent(_event, _x+X, _y+Y, ID);
+        textBox.handleEvent(_event, _x+X, _y+Y, ID, _mID);
     }
     else if (Type == "canvas")
     {
-        canvas.handleEvent(_event, _x+X, _y+Y, ID);
+        canvas.handleEvent(_event, _x+X, _y+Y, ID, _mID);
     }
 }
-void GUIElement::handleSoftEvent(std::list<std::string> _args, int _x, int _y)
+void GUIElement::handleSoftEvent(std::list<std::string> _args, int _x, int _y, std::string _mID)
 {
     if(Type == "codebox")
     {
-        codeBox.handleSoftEvent(_args, _x+X, _y+Y, ID);
+        codeBox.handleSoftEvent(_args, _x+X, _y+Y, ID, _mID);
     }
     else if (Type == "button")
     {
-        button.handleSoftEvent(_args, _x+X, _y+Y, ID);
+        button.handleSoftEvent(_args, _x+X, _y+Y, ID, _mID);
     }
     else if (Type == "horslider")
     {
-        horSlider.handleSoftEvent(_args, _x+X, _y+Y, ID);
+        horSlider.handleSoftEvent(_args, _x+X, _y+Y, ID, _mID);
     }
     else if (Type == "vertslider")
     {
-        vertSlider.handleSoftEvent(_args, _x+X, _y+Y, ID);
+        vertSlider.handleSoftEvent(_args, _x+X, _y+Y, ID, _mID);
     }
     else if (Type == "label")
     {
-        label.handleSoftEvent(_args, _x+X, _y+Y, ID);
+        label.handleSoftEvent(_args, _x+X, _y+Y, ID, _mID);
     }
     else if (Type == "textbox")
     {
-        textBox.handleSoftEvent(_args, _x+X, _y+Y, ID);
+        textBox.handleSoftEvent(_args, _x+X, _y+Y, ID, _mID);
     }
     else if (Type == "canvas")
     {
-        canvas.handleSoftEvent(_args, _x+X, _y+Y, ID);
+        canvas.handleSoftEvent(_args, _x+X, _y+Y, ID, _mID);
     }
 }
-void GUIElement::handleTask(std::list<std::string> _args, int _x, int _y)
+void GUIElement::handleTask(std::list<std::string> _args, int _x, int _y, std::string _mID)
 {
     if(Type == "codebox")
     {
-        codeBox.handleTask(_args, _x+X, _y+Y, ID);
+        codeBox.handleTask(_args, _x+X, _y+Y, ID, _mID);
     }
     else if (Type == "button")
     {
-        button.handleTask(_args, _x+X, _y+Y, ID);
+        button.handleTask(_args, _x+X, _y+Y, ID, _mID);
     }
     else if (Type == "horslider")
     {
-        horSlider.handleTask(_args, _x+X, _y+Y, ID);
+        horSlider.handleTask(_args, _x+X, _y+Y, ID, _mID);
     }
     else if (Type == "vertslider")
     {
-        vertSlider.handleTask(_args, _x+X, _y+Y, ID);
+        vertSlider.handleTask(_args, _x+X, _y+Y, ID, _mID);
     }
     else if (Type == "label")
     {
-        label.handleTask(_args, _x+X, _y+Y, ID);
+        label.handleTask(_args, _x+X, _y+Y, ID, _mID);
     }
     else if (Type == "textbox")
     {
-        textBox.handleTask(_args, _x+X, _y+Y, ID);
+        textBox.handleTask(_args, _x+X, _y+Y, ID, _mID);
     }
     else if (Type == "canvas")
     {
-        canvas.handleTask(_args, _x+X, _y+Y, ID);
+        canvas.handleTask(_args, _x+X, _y+Y, ID, _mID);
     }
 }
