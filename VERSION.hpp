ee57c4e6492078198b461462c3b5029f45ffb798
InDieTasten <indietasten@gmail.com>
2014-09-24 20:35:00 +0200

    Change version
    Former-commit-id: 11805c5eeb5e1df66120f006cbd4dde30f7aa7a5

--- VERSION.hpp
+++ VERSION.hpp
@@ -6,7 +6,7 @@
 namespace VERSION
 {
     std::string name = "[[Untitled Game]]";
-    std::string version = "v0.9.3-pre-alpha";
+    std::string version = "v0.9.4-pre-alpha (DEVELOPMENT VERSION)";
 }
 
 #endif // _VERSION_hpp_
