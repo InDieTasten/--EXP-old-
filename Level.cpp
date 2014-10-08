a84c756db76ab433025ef599591604142d7152dc
InDieTasten <indietasten@gmail.com>
2014-10-08 17:28:56 +0200

    Default level
    Former-commit-id: 82d739426a9cb3d8e62cb701363d688e71de97c9

--- Level.cpp
+++ Level.cpp
@@ -1,6 +1,24 @@
 #include "Level.hpp"
 
 //CONSTRUCTORS
+Level::Level()
+{
+
+    SolarSystem x;
+    x.Description = "default";
+    x.ID = "default";
+    x.Name = "default";
+
+    SpaceObject obj;
+    obj.ID = "default";
+    obj.Position = Vector(0.0, 0.0);
+    obj.Velocity = Vector(0.0, 0.0);
+    obj.Mass = 15.0;
+    obj.TextureID = "$_missing";
+
+    x.SpaceObjectList.push_back(obj);
+    activeSystem = x;
+}
 
 //DESTRUCTORS
 
