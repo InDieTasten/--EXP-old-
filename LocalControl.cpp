4c3985b36b1f61cd32776a8ee439291761b67e3b
InDieTasten <indietasten@gmail.com>
2014-10-08 23:00:44 +0200

    action interpretation
    Former-commit-id: 779f2240a7e244f0ae9729c1168b7b445aa493e7 [formerly 7dea4bc4a66ff2210a81aa3f65889b4f88ab6eb5]
    Former-commit-id: b42366a82baa21a57b221289c283a1925d2791c8

--- LocalControl.cpp
+++ LocalControl.cpp
@@ -2,8 +2,6 @@
 
 LocalControl::LocalControl()
 {
-    targetRotation   = 0.0;
-
     rotateTarget     = 0.0;
     translateForward = 0.0;
     translateBack    = 0.0;
@@ -12,5 +10,44 @@ LocalControl::LocalControl()
 }
 void LocalControl::analyzeAction(std::list<std::string> _action)
 {
-
+    //assume "action" already cut away
+    if(*_action.begin() == "targetPoint")
+    {
+        _action.pop_front();
+        targetPoint.x = *_action.begin();
+        _action.pop_front();
+        targetPoint.y = *_action.begin();
+    }
+    else if(*_action.begin() == "startTranslateForward")
+    {
+        translateForward = 100.0;
+    }
+    else if(*_action.begin() == "endTranslateForward")
+    {
+        translateForward = 0.0;
+    }
+    else if(*_action.begin() == "startTranslateBack")
+    {
+        translateBack = 100.0;
+    }
+    else if(*_action.begin() == "endTranslateBack")
+    {
+        translateBack = 0.0;
+    }
+    else if(*_action.begin() == "startTranslateLeft")
+    {
+        translateLeft = 100.0;
+    }
+    else if(*_action.begin() == "endTranslateLeft")
+    {
+        translateLeft = 0.0;
+    }
+    else if(*_action.begin() == "startTranslateRight")
+    {
+        translateRight = 100.0;
+    }
+    else if(*_action.begin() == "endTranslateRight")
+    {
+        translateRight = 0.0;
+    }
 }
