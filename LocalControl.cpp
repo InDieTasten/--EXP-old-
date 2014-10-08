8cfebff3cf938173c7450bf4e9674529382082d9
InDieTasten <indietasten@gmail.com>
2014-10-08 22:45:56 +0200

    Control structure
    Former-commit-id: d297f3caf48d19d4d191cd72dfc171515797ad58

--- LocalControl.cpp
+++ LocalControl.cpp
@@ -1,1 +1,16 @@
+#include "LocalControl.hpp"
 
+LocalControl::LocalControl()
+{
+    targetRotation   = 0.0;
+
+    rotateTarget     = 0.0;
+    translateForward = 0.0;
+    translateBack    = 0.0;
+    translateLeft    = 0.0;
+    translateRight   = 0.0;
+}
+void LocalControl::analyzeAction(std::list<std::string> _action)
+{
+
+}
--- LocalControl.hpp
+++ LocalControl.hpp
@@ -1,1 +1,17 @@
+#ifndef _LocalControl_hpp_
+#define _LocalControl_hpp_
 
+class LocalControl
+{
+public:
+    LocalControl();
+    analyzeAction(std::list<std::string> _action);
+
+    float rotateTarget;
+    float translateForward;
+    float translateBack;
+    float translateLeft;
+    float translateRight;
+};
+
+#endif // _LocalControl_hpp_
