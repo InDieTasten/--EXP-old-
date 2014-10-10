cbda49096170a83e5a8d0f17d2702177c3a63a6e
InDieTasten <indietasten@gmail.com>
2014-10-10 12:02:33 +0200

    Vector mathematics
    Former-commit-id: c4c714d05efa37dc886b9679284365425c49e925

--- Vector.cpp
+++ Vector.cpp
@@ -10,7 +10,36 @@ Vector::Vector(double _x, double _y)
     x = _x;
     y = _y;
 }
+Vector::Vector(sf::Vector2f _v)
+{
+    x = _v.x;
+    y = _v.y;
+}
 
 //DESTRUCTORS
 
 //METHODS
+Vector Vector::operator+(const Vector&  other)
+{
+    double result_x = x + other.x;
+    double result_y = y + other.y;
+    return Vector( x, y);
+}
+Vector Vector::operator-(const Vector&  other)
+{
+    double result_x = x - other.x;
+    double result_y = y - other.y;
+    return Vector( x, y);
+}
+Vector Vector::operator*(const Vector&  other)
+{
+    double result_x = x * other.x;
+    double result_y = y * other.y;
+    return Vector( x, y);
+}
+Vector Vector::operator/(const Vector&  other)
+{
+    double result_x = x / other.x;
+    double result_y = y / other.y;
+    return Vector( x, y);
+}
