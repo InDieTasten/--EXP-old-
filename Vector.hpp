0797ef0ed934e8c15352c13b414a84ee921525b2
InDieTasten <indietasten@gmail.com>
2014-10-10 12:02:33 +0200

    Vector mathematics
    Former-commit-id: cbda49096170a83e5a8d0f17d2702177c3a63a6e [formerly c4c714d05efa37dc886b9679284365425c49e925]
    Former-commit-id: a448992c931741c39b7b0127fb824c835d528b22

--- Vector.hpp
+++ Vector.hpp
@@ -1,6 +1,8 @@
 #ifndef _Vector_hpp_
 #define _Vector_hpp_
 
+#include <SFML/Graphics.hpp>
+
 class Vector
 {
     //MEMBERS
@@ -12,10 +14,15 @@ public:
     //CONSTRUCTORS
     Vector();
     Vector(double _x, double _y);
+    Vector(sf::Vector2f);
 
     //DESTRUCTORS
 
     //METHODS
+    Vector operator+(const Vector&  other);
+    Vector operator-(const Vector&  other);
+    Vector operator*(const Vector&  other);
+    Vector operator/(const Vector&  other);
 };
 
 #endif
