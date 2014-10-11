1c89e64d333c975db73748497adb75b6ee615721
InDieTasten <indietasten@gmail.com>
2014-10-11 13:43:56 +0200

    Moving slider effect fix

--- VertSlider.cpp
+++ VertSlider.cpp
@@ -70,17 +70,17 @@ void VertSlider::Update(int _x, int _y, std::string _id, std::string _mID)
             incBox.setOutlineThickness(1.0f);
             incBox.setOutlineColor(ButtonBorderActive);
         }
-        if(hoverSlide)
+        if(moveSlide)
         {
-            if(moveSlide)
-            {
-                slider.setPosition((float)_x, (float) 2 + _y + Width+((ratio*(Height-2*Width))/2) + ((value-min)/(max-min))*(Height-2*Width-(ratio*(Height-2*Width))) - (ratio*(Height-2*Width))/2);
-                slider.setSize(sf::Vector2f((float)_x+Width - (_x), (float) (-2) + _y + Width+((ratio*(Height-2*Width))/2) + ((value-min)/(max-min))*(Height-2*Width-(ratio*(Height-2*Width))) + (ratio*(Height-2*Width))/2 - (2 + _y + Width+((ratio*(Height-2*Width))/2) + ((value-min)/(max-min))*(Height-2*Width-(ratio*(Height-2*Width))) - (ratio*(Height-2*Width))/2)));
-                slider.setFillColor(SliderBackgroundMoving);
-                slider.setOutlineThickness(1.0f);
-                slider.setOutlineColor(SliderBorderMoving);
-            }
-            else
+            slider.setPosition((float)_x, (float) 2 + _y + Width+((ratio*(Height-2*Width))/2) + ((value-min)/(max-min))*(Height-2*Width-(ratio*(Height-2*Width))) - (ratio*(Height-2*Width))/2);
+            slider.setSize(sf::Vector2f((float)_x+Width - (_x), (float) (-2) + _y + Width+((ratio*(Height-2*Width))/2) + ((value-min)/(max-min))*(Height-2*Width-(ratio*(Height-2*Width))) + (ratio*(Height-2*Width))/2 - (2 + _y + Width+((ratio*(Height-2*Width))/2) + ((value-min)/(max-min))*(Height-2*Width-(ratio*(Height-2*Width))) - (ratio*(Height-2*Width))/2)));
+            slider.setFillColor(SliderBackgroundMoving);
+            slider.setOutlineThickness(1.0f);
+            slider.setOutlineColor(SliderBorderMoving);
+        }
+        else
+        {
+            if(hoverSlide)
             {
                 slider.setPosition((float)_x, (float) 2 + _y + Width+((ratio*(Height-2*Width))/2) + ((value-min)/(max-min))*(Height-2*Width-(ratio*(Height-2*Width))) - (ratio*(Height-2*Width))/2);
                 slider.setSize(sf::Vector2f((float)_x+Width - (_x), (float) (-2) + _y + Width+((ratio*(Height-2*Width))/2) + ((value-min)/(max-min))*(Height-2*Width-(ratio*(Height-2*Width))) + (ratio*(Height-2*Width))/2 - (2 + _y + Width+((ratio*(Height-2*Width))/2) + ((value-min)/(max-min))*(Height-2*Width-(ratio*(Height-2*Width))) - (ratio*(Height-2*Width))/2)));
@@ -88,14 +88,15 @@ void VertSlider::Update(int _x, int _y, std::string _id, std::string _mID)
                 slider.setOutlineThickness(1.0f);
                 slider.setOutlineColor(SliderBorderHover);
             }
-        }
-        else
-        {
-            slider.setPosition((float)_x, (float) 2 + _y + Width+((ratio*(Height-2*Width))/2) + ((value-min)/(max-min))*(Height-2*Width-(ratio*(Height-2*Width))) - (ratio*(Height-2*Width))/2);
-            slider.setSize(sf::Vector2f((float)_x+Width - (_x), (float) (-2) + _y + Width+((ratio*(Height-2*Width))/2) + ((value-min)/(max-min))*(Height-2*Width-(ratio*(Height-2*Width))) + (ratio*(Height-2*Width))/2 - (2 + _y + Width+((ratio*(Height-2*Width))/2) + ((value-min)/(max-min))*(Height-2*Width-(ratio*(Height-2*Width))) - (ratio*(Height-2*Width))/2)));
-            slider.setFillColor(SliderBackgroundActive);
-            slider.setOutlineThickness(1.0f);
-            slider.setOutlineColor(SliderBorderActive);
+            else
+            {
+                slider.setPosition((float)_x, (float) 2 + _y + Width+((ratio*(Height-2*Width))/2) + ((value-min)/(max-min))*(Height-2*Width-(ratio*(Height-2*Width))) - (ratio*(Height-2*Width))/2);
+                slider.setSize(sf::Vector2f((float)_x+Width - (_x), (float) (-2) + _y + Width+((ratio*(Height-2*Width))/2) + ((value-min)/(max-min))*(Height-2*Width-(ratio*(Height-2*Width))) + (ratio*(Height-2*Width))/2 - (2 + _y + Width+((ratio*(Height-2*Width))/2) + ((value-min)/(max-min))*(Height-2*Width-(ratio*(Height-2*Width))) - (ratio*(Height-2*Width))/2)));
+                slider.setFillColor(SliderBackgroundActive);
+                slider.setOutlineThickness(1.0f);
+                slider.setOutlineColor(SliderBorderActive);
+            }
+
         }
         bar.setPosition((float)_x+Width/2-2, (float)_y+7);
         bar.setSize(sf::Vector2f((float)_x+Width/2+2-(_x+Width/2-2), (float)_y + Height-7-(_y+7)));
