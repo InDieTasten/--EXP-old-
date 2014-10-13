4c341201bc28115e93b18f4784ce7a241f61fb14
InDieTasten <indietasten@gmail.com>
2014-10-14 01:24:39 +0200

    Object saving
    Former-commit-id: 20d3818de79dae20040ed010ab4d45e069832167

--- SpaceObject.cpp
+++ SpaceObject.cpp
@@ -11,31 +11,14 @@ SpaceObject::SpaceObject()
 //DESTRUCTORS
 
 //METHODS
-void SpaceObject::handleSoftEvent(std::list<std::string> _args)
-{
 
-}
-void SpaceObject::handleTask(std::list<std::string> _args)
+void SpaceObject::saveToStream(std::ofstream* output)
 {
-
-}
-
-void SpaceObject::loadme(std::ifstream* filehandle)
-{
-    /*filehandle->read((char*) &Controller,sizeof(Actor));
-    filehandle->read((char*) &ID,sizeof(std::string));
-    filehandle->read((char*) &Position,sizeof(Vector));
-    filehandle->read((char*) &Rotation,sizeof(float));
-    filehandle->read((char*) &Velocity,sizeof(Vector));
-    filehandle->read((char*) &Mass,sizeof(double));
-    filehandle->read((char*) &TextureID,sizeof(std::string));*/
-}
-void SpaceObject::saveme(std::ofstream* filehandle)
-{
-    /*filehandle->write((char*) &Controller,sizeof(Actor));
-    filehandle->write((char*) &ID,sizeof(std::string));
-    filehandle->write((char*) &Position,sizeof(Vector));
-    filehandle->write((char*) &Rotation,sizeof(float));
-    filehandle->write((char*) &Velocity,sizeof(Vector));
-    filehandle->write((char*) &Mass,sizeof(double));*/
+    output->write((char*) &ID, sizeof(ID));
+    output->write((char*) &Rotation, sizeof(Rotation));
+    output->write((char*) &Mass, sizeof(Mass));
+    output->write((char*) &TextureID, sizeof(TextureID));
+    output->write((char*) &flyByLocal, sizeof(flyByLocal));
+    Position.saveToStream(output);
+    Velocity.saveToStream(output);
 }
