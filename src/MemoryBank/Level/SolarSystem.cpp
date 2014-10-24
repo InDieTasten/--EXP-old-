2e2bb38718fd1f00ddd8b418a77184bb3629d836
InDieTasten <indietasten@gmail.com>
2014-10-24 12:25:13 +0200

    saving checkpoint 2
    Former-commit-id: 15f121b9be75f830f2eef254679dcffd23c6a844

--- src/MemoryBank/Level/SolarSystem.cpp
+++ src/MemoryBank/Level/SolarSystem.cpp
@@ -9,7 +9,8 @@ void SolarSystem::saveToStream(std::ofstream& output)
     output.write((char*) &ID, sizeof(ID));
     output.write((char*) &Name, sizeof(Name));
     output.write((char*) &Description, sizeof(Description));
-    output.write((char*) SpaceObjectList.size(), sizeof(SpaceObjectList.size()));
+    unsigned int x = SpaceObjectList.size();
+    output.write((char*) &x, sizeof(x));
     for(std::list<SpaceObject>::iterator it = SpaceObjectList.begin(); it != SpaceObjectList.end(); it++)
     {
         it->saveToStream(output);
