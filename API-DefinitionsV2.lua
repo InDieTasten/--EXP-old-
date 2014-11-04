24ed5a849f752ae39376be3be9cb512c9d07a4b0
InDieTasten <indietasten@gmail.com>
2014-11-05 00:57:37 +0100

    moddingAPIv2 (final) + definition list

---
+++ API-DefinitionsV2.lua
@@ -0,0 +1,37 @@
+l = getLevel() --
+	l["name"] = "name of the level"
+	l["description"] = "description of the level"
+
+syss = getSystems() --
+	syss[1] = "systemID1"
+	syss[2] = "systemID2"
+
+selectSystem("systemID1") --
+
+sys = getSystem("systemID1" || selected) --
+	sys["id"] = "systemID1"
+	sys["name"] = "systemName1"
+	sys["description"] = "description of system"
+
+objs = getObjects("systemID2" || selected) --
+	objs[1] = "objName1"
+	objs[2] = "objName2"
+
+obj = getObject("systemID2" || selected, "objName1") --
+	obj["name"] = "objName1"
+	obj["description"] = "description of object"
+	obj["rotation"] = 270
+	obj["x"] = 0.5
+	obj["y"] = 0.5
+	obj["dx"] = 1.5
+	obj["dy"] = 1.5
+	obj["textureID"] = "$_missing"
+	obj["mass"] = 20.4
+
+setObject("systemID2" || selected, obj) --
+
+removeObject("systemID2" || selected, obj || "objName1") 
+
+x, y = getGameMouse() --
+
+x, y = getGuiMouse()
