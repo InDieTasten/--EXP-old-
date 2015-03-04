363a13c076541efbd75347945e32ed04d093bee7
InDieTasten <indietasten@gmail.com>
2015-03-04 23:00:51 +0100

    SolarSystem header
    Former-commit-id: efda1efbcf609be2b07675cf73ebd39e03987921

---
+++ include/Level/SolarSystem.hpp
@@ -0,0 +1,29 @@
+#ifndef _SolarSystem_hpp_
+#define _SolarSystem_hpp_
+
+#include <Level\Level.hpp>
+#include <list>
+#include <Level\Planet.hpp>
+#include <Level\Bullet.hpp>
+#include <Level\Construction.hpp>
+#include <Level\Ship.hpp>
+#include <Level\Asteroid.hpp>
+#include <Level\Debris.hpp>
+
+class SolarSystem
+{
+private:
+	Level* parent;
+
+	std::list<Planet>* planets;
+	std::list<Bullet>* bullets;
+	std::list<Construction>* constructs;
+	std::list<Ship>* ships;
+	std::list<Asteroid>* asteroids;
+	std::list<Debris>* debris;
+
+public:
+
+};
+
+#endif // !_SolarSystem_hpp_
