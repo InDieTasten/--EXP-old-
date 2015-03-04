87b32b264bad345ea611224b176c0ee1dad86731
InDieTasten <indietasten@gmail.com>
2015-03-04 22:27:45 +0100

    Asset header
    Former-commit-id: 5f3aa9e8adcbb7b21e836686e75701878b31c256

---
+++ include/Assets/Asset.hpp
@@ -0,0 +1,21 @@
+#ifndef _Asset_hpp_
+#define _Asset_hpp_
+
+#include <string>
+
+using namespace std;
+
+class Asset
+{
+protected:
+	string* id;
+	string* path;
+	sf::Clock* lastget;
+
+public:
+	Asset();
+	Asset(string _id, string _path);
+	~Asset();
+};
+
+#endif // !_Asset_hpp_
