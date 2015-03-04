835d36e491152504508dbc3125e26497b02b5e81
InDieTasten <indietasten@gmail.com>
2015-03-04 21:26:49 +0100

    I think I can start rewriting
    Former-commit-id: 2b87290a2f5e09d14f0215c1181e3a39c027b441

--- src/Core/main.cpp
+++ src/Core/main.cpp
@@ -21,7 +21,7 @@ int main(int argc, char *argv[])
 	ug::log("Game is launching");
 	sf::RenderWindow App(sf::VideoMode(1280, 720, 32), VERSION::name + " " + VERSION::version, sf::Style::Titlebar | sf::Style::Resize | sf::Style::Close);
 	
-
+	
 
 	while (App.isOpen())
 	{
@@ -32,7 +32,7 @@ int main(int argc, char *argv[])
 		}
 	}
 
-	printf("\n=|=|=|=|=|=|=|=|=|=|=|=|=|=\n\nThe game closed. If you see errors above, please contact the developers");
+	ug::log("Game is done");
 	getchar();
 	return EXIT_SUCCESS;
 }
