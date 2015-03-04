ddc9cb1d2b104d72bbaa8f9090fb9821ff8dd5dc
InDieTasten <indietasten@gmail.com>
2015-03-04 23:09:11 +0100

    SolarSystem header update

--- include/Level/SolarSystem.hpp
+++ include/Level/SolarSystem.hpp
@@ -3,27 +3,30 @@
 
 #include <Level\Level.hpp>
 #include <list>
-//#include <Level\Planet.hpp>
-//#include <Level\Bullet.hpp>
-//#include <Level\Construction.hpp>
-//#include <Level\Ship.hpp>
-//#include <Level\Asteroid.hpp>
-//#include <Level\Debris.hpp>
-
+#include <Level\Planet.hpp>
+#include <Level\Bullet.hpp>
+#include <Level\Construction.hpp>
+#include <Level\Ship.hpp>
+#include <Level\Asteroid.hpp>
+#include <Level\Debris.hpp>
+#include <Core\ThreadManager.hpp>
 class SolarSystem
 {
 private:
 	Level* parent;
 
-	//std::list<Planet>* planets;
-	//std::list<Bullet>* bullets;
-	//std::list<Construction>* constructs;
-	//std::list<Ship>* ships;
-	//std::list<Asteroid>* asteroids;
-	//std::list<Debris>* debris;
+	std::list<Planet>* planets;
+	std::list<Bullet>* bullets;
+	std::list<Construction>* constructs;
+	std::list<Ship>* ships;
+	std::list<Asteroid>* asteroids;
+	std::list<Debris>* debris;
+
+	ThreadManager* threadManager;
 
 public:
-
+	SolarSystem();
+	~SolarSystem();
 };
 
 #endif // !_SolarSystem_hpp_
