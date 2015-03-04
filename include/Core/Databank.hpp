935f6afcecdf7cc3af286d85d625db48b3502bff
InDieTasten <indietasten@gmail.com>
2015-03-04 22:26:49 +0100

    Databank header
    Former-commit-id: 352cc4a914afe5c67ad09cc45eca60b7d8eb2189 [formerly 192c0d2f202a8d4db30933f63a6cfca55a0b0dea]
    Former-commit-id: 6556cb43b08fac9b50160290509bfcccbd4a6d3b

---
+++ include/Core/Databank.hpp
@@ -0,0 +1,55 @@
+#ifndef _Databank_hpp_
+#define _Databank_hpp_
+
+#include <map>
+#include <string>
+#include <Assets/Texture.hpp>
+#include <Assets/Font.hpp>
+#include <Assets/Sound.hpp>
+#include <Assets/Music.hpp>
+#include <Assets/Video.hpp>
+#include <Level/Level.hpp>
+#include <Core/ThreadManager.hpp>
+#include <SFML\Audio.hpp>
+
+using namespace std;
+
+class Databank
+{
+private:
+	std::map<string, Texture> *textures;
+	std::map<string, Font> *fonts;
+	std::map<string, Sound> *sounds;
+	std::map<string, Music> *tracks;
+	std::map<string, Video> *videos;
+
+	Level* level;
+	ThreadManager* threadManager;
+	GUIManager* guiManager;
+public:
+	Databank();
+	~Databank();
+
+	void addTexture(string _id, string _path);
+	void removeTexture(string _id);
+	sf::Texture* getTexture(string _id);
+
+	void addFont(string _id, string _path);
+	void removeFont(string _id);
+	sf::Font* getFont(string _id);
+
+	void addSound(string _id, string _path);
+	void removeSound(string _id);
+	sf::Sound* getSound(string _id);
+
+	void addMusic(string _id, string _path);
+	void removeMusic(string _id);
+	sf::Music* getMusic(string _id);
+
+	void addVideo(string _id, string _path);
+	void removeVideo(string _id);
+	Video* getVideo(string _id);
+
+};
+
+#endif // !_Databank_hpp_
