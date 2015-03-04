b9c6332aeb73a10950efb3acf75e9f141abfcede
InDieTasten <indietasten@gmail.com>
2015-03-04 22:28:07 +0100

    Font header
    Former-commit-id: 997fe4a70e5fbb793a18142d464bf5190e6dfd31

---
+++ include/Assets/Font.hpp
@@ -0,0 +1,21 @@
+#ifndef _Font_hpp_
+#define _Font_hpp_
+
+#include <Assets\Asset.hpp>
+#include <string>
+#include <SFML\Graphics.hpp>
+
+using namespace std;
+
+class Font : public Asset
+{
+private:
+	sf::Font* data;
+
+public:
+	sf::Font* getFont();
+	void load();
+	void unload();
+};
+
+#endif // !_Font_hpp_
