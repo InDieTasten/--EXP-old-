a6d42546dd39505cd79773218337e8866112934d
InDieTasten <indietasten@gmail.com>
2015-03-05 07:05:19 +0100

    PhysicalInstance header

---
+++ include/Level/PhysicalInstance.hpp
@@ -0,0 +1,28 @@
+#ifndef _PhysicalInstance_hpp_
+#define _PhysicalInstance_hpp_
+
+#include <Level\Vector.hpp>
+#include <SFML\System.hpp>
+#include <Level\CollisionMesh.hpp>
+
+class PhysicalInstance
+{
+private:
+	Vector* position;
+	Vector* velocity;
+
+	Vector* gravityForce;
+	sf::Clock lastupdate;
+
+	Vector* ghostForce;
+	double* mass;
+
+	CollisionMesh* collMesh;
+
+public:
+	PhysicalInstance();
+	~PhysicalInstance();
+
+};
+
+#endif // !_PhysicalInstance_hpp_
