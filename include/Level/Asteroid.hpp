03f26d96983f6c3729e4ded79c0900a646b710f7
InDieTasten <indietasten@gmail.com>
2015-03-05 23:21:19 +0100

    Asteroid header
    Former-commit-id: 71d11daaaa912a4df99a1cbcc3136f6ea8da1304

---
+++ include/Level/Asteroid.hpp
@@ -0,0 +1,19 @@
+#ifndef _Asteroid_hpp_
+#define _Asteroid_hpp_
+
+#include <Level\SolarSystem.hpp>
+#include <string>
+#include <Level\PhysicalInstance.hpp>
+
+class Asteroid
+{
+private:
+	SolarSystem* parent;
+	string* ID;
+	PhysicalInstance* phInstance;
+public:
+	Asteroid();
+	~Asteroid();
+};
+
+#endif // !_Asteroid_hpp_
