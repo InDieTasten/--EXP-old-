3915ad7c5b38475de6d5ea322c05bed17c9576cb
InDieTasten <indietasten@gmail.com>
2015-03-05 07:08:26 +0100

    Vector header
    Former-commit-id: 9dd3fcb5faa350ce9b1bcb7ac19798a65970d86e

---
+++ include/Level/Vector.hpp
@@ -0,0 +1,19 @@
+#ifndef _Vector_hpp_
+#define _Vector_hpp_
+
+class Vector
+{
+public:
+	double X;
+	double Y;
+	float R;
+
+	Vector();
+	Vector(double, double);
+	Vector(double, double, float);
+	~Vector();
+
+	//operator
+};
+
+#endif // !_Vector_hpp_
