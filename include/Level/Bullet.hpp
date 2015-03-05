9464d9c6f68476d2de4592e6c429c1804f16f3f8
InDieTasten <indietasten@gmail.com>
2015-03-05 22:52:21 +0100

    Bullet header update
    Former-commit-id: 55e38f8d18c1fcf5a468f465f2bfa2873b3ef8e5 [formerly 7251122c7a7bb544cc56c49a275e2c1ed049fbd8]
    Former-commit-id: d1c0403ef9d2b56fa1294bf3a8a90ac19f63f4c9

--- include/Level/Bullet.hpp
+++ include/Level/Bullet.hpp
@@ -2,12 +2,14 @@
 #define _Bullet_hpp_
 
 #include <Level\SolarSystem.hpp>
+#include <string>
 #include <Level\PhysicalInstance.hpp>
 
 class Bullet
 {
 private:
 	SolarSystem* parent;
+	string* ID;
 	PhysicalInstance* phInstance;
 	//Weapon* source;
 
