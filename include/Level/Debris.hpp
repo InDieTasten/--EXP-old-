b375340e6f6f25a760817849994f2437a18e7428
InDieTasten <indietasten@gmail.com>
2015-03-05 23:39:33 +0100

    Debris header

---
+++ include/Level/Debris.hpp
@@ -0,0 +1,22 @@
+#ifndef _Debris_hpp_
+#define _Debris_hpp_
+
+#include <Level\SolarSystem.hpp>
+#include <string>
+#include <Level\PhysicalInstance.hpp>
+
+class Debris
+{
+private:
+	SolarSystem* parent;
+	string* ID;
+	PhysicalInstance* phInstance;
+
+	//Item* item
+
+public:
+	Debris();
+	~Debris();
+};
+
+#endif // !_Debris_hpp_
