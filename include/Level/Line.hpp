067fd7a969d5138fdaddca8c2730bb3e6b844b67
InDieTasten <indietasten@gmail.com>
2015-03-05 21:27:02 +0100

    Line header

---
+++ include/Level/Line.hpp
@@ -0,0 +1,19 @@
+#ifndef _Line_hpp_
+#define _Line_hpp_
+
+#include <Level\Vector.hpp>
+
+class Line
+{
+private:
+	Vector* start;
+	Vector* end;
+
+public:
+	Line();
+	Line(Vector* _start, Vector* _end);
+	Line(double*, double*, double*, double*);
+	~Line();
+};
+
+#endif // !_Line_hpp_
