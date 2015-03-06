18de895df83745887cf12c5943bed95a50251ca9
InDieTasten <indietasten@gmail.com>
2015-03-07 00:59:37 +0100

    Databank header doc
    Former-commit-id: 192193d9ac9fc2b9e0e1137719425aeb5af107a0

--- include/Core/Databank.hpp
+++ include/Core/Databank.hpp
@@ -14,14 +14,18 @@
 
 using namespace std;
 
+/// <summary>
+/// Class holding pretty much the entire game state.
+/// Also handles resource management and memory usage
+/// </summary>
 class Databank
 {
 private:
-	map<string*, Texture*>* textures;
-	map<string*, Font*>* fonts;
-	map<string*, Sound*>* sounds;
-	map<string*, Music*>* tracks;
-	map<string*, Video*>* videos;
+	map<string, Texture*> textures;
+	map<string, Font*> fonts;
+	map<string, Sound*> sounds;
+	map<string, Music*> tracks;
+	map<string, Video*> videos;
 
 	sf::RenderWindow* renderWindow;
 	Level* level;
@@ -31,25 +35,31 @@ public:
 	Databank(sf::RenderWindow* _window);
 	~Databank();
 
-	void addTexture(string* _id, string* _path);
+	void addTexture(string _id, string _path);
 	void removeTexture(string* _id);
 	sf::Texture* getTexture(string* _id);
 
-	void addFont(string* _id, string* _path);
+	void addFont(string _id, string _path);
 	void removeFont(string* _id);
 	sf::Font* getFont(string* _id);
 
-	void addSound(string* _id, string* _path);
+	void addSound(string _id, string _path);
 	void removeSound(string* _id);
 	sf::Sound* getSound(string* _id);
 
-	void addMusic(string* _id, string* _path);
+	void addMusic(string _id, string _path);
 	void removeMusic(string* _id);
 	sf::Music* getMusic(string* _id);
 
-	void addVideo(string* _id, string* _path);
+	void addVideo(string _id, string _path);
 	void removeVideo(string* _id);
 	Video* getVideo(string* _id);
+
+	/// <summary>
+	/// Forces the databank to unload all resources that haven't been used in a specified time
+	/// </summary>
+	/// <param name="_access">Specifies the time, at which a resource gets unloaded</param>
+	void shrink(sf::Time _access);
 
 };
 
