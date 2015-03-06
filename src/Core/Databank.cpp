7be1ad5ec7ae11b7a82a3ed79b58b50c53e0ea65
InDieTasten <indietasten@gmail.com>
2015-03-06 20:04:38 +0100

    Databank source constructor
    Former-commit-id: 094561a839266dee3bfafba6ae4ff1428fafb33c

--- src/Core/Databank.cpp
+++ src/Core/Databank.cpp
@@ -2,5 +2,15 @@
 
 Databank::Databank(sf::RenderWindow* _window)
 {
+	//pointer
 	renderWindow = _window;
+
+	//construct real data
+	textures = new map<string*, Texture*>();
+	fonts = new map<string*, Font*>();
+	sounds = new map<string*, Sound*>();
+	tracks = new map<string*, Music*>();
+	videos = new map<string*, Video*>();
+	level = new Level();
+	guiManager = new GUIManager();
 }
