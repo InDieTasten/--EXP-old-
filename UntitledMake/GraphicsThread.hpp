a9d42fb52782686242841734a3b2d12c29ee54f3
InDieTasten <indietasten@gmail.com>
2015-03-06 15:13:02 +0100

    GraphicsThread header update
    Former-commit-id: 2b3519d7d3a81746eebff886a061e60a20d3ae9a [formerly 6e6a4000d06ddd8f00092ecfbe1319c4376ecd75]
    Former-commit-id: 8787645dc2962467a71e4662d801ab9e8c3dcfa9

--- UntitledMake/GraphicsThread.hpp
+++ UntitledMake/GraphicsThread.hpp
@@ -1,12 +1,14 @@
 #ifndef _GraphicsThread_hpp_
 #define _GraphicsThread_hpp_
 
+#include <Core\ThreadManager.hpp>
+
 using namespace std;
 
 class GraphicsThread
 {
 private:
-
+	ThreadManager* parent;
 public:
 	GraphicsThread();
 	~GraphicsThread();
