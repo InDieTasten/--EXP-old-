993eb8a80fc390b6387e35eb6ee35c6ae2c4539d
InDieTasten <indietasten@gmail.com>
2015-03-06 15:11:28 +0100

    MovementThread header
    Former-commit-id: bec03533bc9ad5135a7a70a2b578ab3de42d9201

---
+++ UntitledMake/MovementThread.hpp
@@ -0,0 +1,15 @@
+#ifndef _MovementThread_hpp_
+#define _MovementThread_hpp_
+
+using namespace std;
+
+class MovementThread
+{
+private:
+
+public:
+	MovementThread();
+	~MovementThread();
+};
+
+#endif // !_MovementThread_hpp_
