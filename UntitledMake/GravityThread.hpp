92c6cf57f182a54bcdc0cfcb402b2b629f374858
InDieTasten <indietasten@gmail.com>
2015-03-06 15:11:05 +0100

    GravityThread  header
    Former-commit-id: 58d436fe9c7a20d0a2d16a32e614b62ad7a7558a

---
+++ UntitledMake/GravityThread.hpp
@@ -0,0 +1,15 @@
+#ifndef _GravityThread_hpp_
+#define _GravityThread_hpp_
+
+using namespace std;
+
+class GravityThread
+{
+private:
+
+public:
+	GravityThread();
+	~GravityThread();
+};
+
+#endif // !_GravityThread_hpp_
