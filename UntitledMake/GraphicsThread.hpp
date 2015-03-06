24ca3f8e6297bc335ef340f49af807982f9dea70
InDieTasten <indietasten@gmail.com>
2015-03-06 15:11:57 +0100

    GraphicsThread header

---
+++ UntitledMake/GraphicsThread.hpp
@@ -0,0 +1,15 @@
+#ifndef _GraphicsThread_hpp_
+#define _GraphicsThread_hpp_
+
+using namespace std;
+
+class GraphicsThread
+{
+private:
+
+public:
+	GraphicsThread();
+	~GraphicsThread();
+};
+
+#endif // !_GraphicsThread_hpp_
