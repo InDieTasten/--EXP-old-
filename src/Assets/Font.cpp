beb2c13cf25d6c8bb5954dc0c5bb6c3e76d8716f
InDieTasten <indietasten@gmail.com>
2015-03-08 14:07:38 +0100

    Font source getFont

--- src/Assets/Font.cpp
+++ src/Assets/Font.cpp
@@ -10,3 +10,16 @@ Font::~Font()
 {
 	ug::log("A Font has been deconstructed");
 }
+sf::Font* Font::getFont()
+{
+	if (data)
+	{
+		lastget.restart();
+		ug::log("The Font '" + *id + "' has been gotton");
+		return data;
+	}
+	else {
+		load();
+		getFont();
+	}
+}
