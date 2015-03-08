c56b79c1999a3c0b0c50e54ed4809eae73a3066e
InDieTasten <indietasten@gmail.com>
2015-03-08 01:01:08 +0100

    ThreadManager header update
    Former-commit-id: ef867e3692cebf0bcf7dabcab4f43831139689c1

--- include/Core/ThreadManager.hpp
+++ include/Core/ThreadManager.hpp
@@ -6,6 +6,7 @@
 #include <Core\MovementThread.hpp>
 #include <list>
 #include <Core\GraphicsThread.hpp>
+#include <thread>
 
 using namespace std;
 extern void ug::log(std::string);
@@ -14,16 +15,18 @@ class ThreadManager
 {
 	SolarSystem* parent;
 	EventThread* eventThread;
-	GravityThread* gravThread;
-	list<MovementThread*>* moveThreads;
-	GraphicsThread* graphThread;
+	GravityThread gravThread;
+	list<MovementThread> moveThreads;
+	GraphicsThread graphThread;
+	thread me;
+	void run();
 
 public:
 	ThreadManager(SolarSystem* _system, EventThread* _main);
 	~ThreadManager();
 
-	void Run();
-	void Stop();
+	void launch();
+	void terminate();
 };
 
 #endif // !_ThreadManager_hpp_
