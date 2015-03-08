265a6b7e2bc3ee578b20d75117ca1691934b8604
InDieTasten <indietasten@gmail.com>
2015-03-08 14:00:17 +0100

    Asset source update

--- src/Assets/Asset.cpp
+++ src/Assets/Asset.cpp
@@ -5,4 +5,9 @@ Asset::Asset(string* _id, string _path)
 	id = _id;
 	path = _path;
 	lastget.restart();
+	ug::log("An asset has been constructed");
+}
+Asset::~Asset()
+{
+	ug::log("An asset has been destructed");
 }
