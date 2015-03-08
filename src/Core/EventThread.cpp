5c880d845de6e8199ddec354621818715fcc35c1
InDieTasten <indietasten@gmail.com>
2015-03-08 16:20:29 +0100

    EventThread chrono shit

--- src/Core/EventThread.cpp
+++ src/Core/EventThread.cpp
@@ -3,7 +3,7 @@
 EventThread::EventThread(sf::RenderWindow* _window)
 {
 	app = _window;
-	looprate = 40.0f;
+	looptime = chrono::milliseconds(1000/50);
 }
 EventThread::~EventThread()
 {
@@ -11,9 +11,15 @@ EventThread::~EventThread()
 }
 void EventThread::run()
 {
-
+	chrono::steady_clock::time_point last = chrono::steady_clock::now();
 	while (app->isOpen())
 	{
+
+		//consumed time:
+		this_thread::sleep_for(chrono::steady_clock::now() - last - looptime);
+		last = chrono::steady_clock::now();
+
+
 		sf::Event Event;
 		while (app->pollEvent(Event))
 		{
