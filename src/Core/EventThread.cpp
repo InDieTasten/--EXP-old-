70ab85a65a8910df943fe10eb413117167bf36f7
InDieTasten <indietasten@gmail.com>
2015-03-08 14:59:23 +0100

    EventThread source update
    Former-commit-id: 72e8466c9ffc8ed9c6f91919d1fd1a70192e02e3 [formerly 13929f7a6501358d8d45b16df779316cffe2863b]
    Former-commit-id: b9f20c88f91fae7db25ff824ce16b01375b782f8

--- src/Core/EventThread.cpp
+++ src/Core/EventThread.cpp
@@ -3,8 +3,21 @@
 EventThread::EventThread(sf::RenderWindow* _window)
 {
 	app = _window;
+	looprate = 40.0f;
 }
 EventThread::~EventThread()
 {
 	app = nullptr;
+}
+void EventThread::run()
+{
+
+	while (app->isOpen())
+	{
+		sf::Event Event;
+		while (app->pollEvent(Event))
+		{
+			//WORK do something with events
+		}
+	}
 }
