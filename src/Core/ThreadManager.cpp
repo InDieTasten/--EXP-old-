baf1bd96f370936934f000dce0f2abbc460b78e2
InDieTasten <indietasten@gmail.com>
2015-03-10 20:54:39 +0100

    ThreadManager source constructor
    Former-commit-id: b74d69059768db0b9d1ebcc04bd5472d57a675d2 [formerly a72884060b715413756709bca6374b38ecd35cdb]
    Former-commit-id: 0f9e3cd1489c0e910e2930f8afd1f803bbf60519

--- src/Core/ThreadManager.cpp
+++ src/Core/ThreadManager.cpp
@@ -1,1 +1,9 @@
 #include <Core\ThreadManager.hpp>
+#include <Level\SolarSystem.hpp>
+
+ThreadManager::ThreadManager(SolarSystem* _parent, EventThread* _main)
+{
+	parent = _parent;
+	eventThread = _main;
+	ug::log("ThreadManager has been constructed: " + *_parent->getID());
+}
