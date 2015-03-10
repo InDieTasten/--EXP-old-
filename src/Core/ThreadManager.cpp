140248a02a745953bc4618b5d49496b5e503aa12
InDieTasten <indietasten@gmail.com>
2015-03-10 20:54:55 +0100

    ThreadManager source destructor
    Former-commit-id: fc730e6574441e5a2087912d6b7e8c1a89440328

--- src/Core/ThreadManager.cpp
+++ src/Core/ThreadManager.cpp
@@ -7,3 +7,12 @@ ThreadManager::ThreadManager(SolarSystem* _parent, EventThread* _main)
 	eventThread = _main;
 	ug::log("ThreadManager has been constructed: " + *_parent->getID());
 }
+ThreadManager::~ThreadManager()
+{
+	string tmp = *parent->getID();
+	//Tell SolarSystem
+	parent = nullptr;
+	//Tell EventThread
+	eventThread = nullptr;
+	ug::log("ThreadManager has been destructed: " + tmp);
+}
