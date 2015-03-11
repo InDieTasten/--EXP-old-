6fb2a96e73949e9b31fe998b04112240c4bab7ed
InDieTasten <indietasten@gmail.com>
2015-03-11 21:33:06 +0100

    GravityThread source constructor

--- src/Core/GravityThread.cpp
+++ src/Core/GravityThread.cpp
@@ -1,1 +1,10 @@
 #include <Core\GravityThread.hpp>
+#include <Core\ThreadManager.hpp>
+
+GravityThread::GravityThread(ThreadManager* _parent)
+{
+	//pointer
+	parent = _parent;
+
+	ug::log("A GravityThread has been constructed: " + *parent->getParent()->getID);
+}
