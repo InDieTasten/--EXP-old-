6acc7b2fc1b0f09711d19e5c0fc0c2509af6ada9
InDieTasten <indietasten@gmail.com>
2015-03-11 21:01:45 +0100

    SolarSystem source constructor

--- src/Level/SolarSystem.cpp
+++ src/Level/SolarSystem.cpp
@@ -1,1 +1,11 @@
 #include <Level\SolarSystem.hpp>
+
+SolarSystem::SolarSystem(Level* _parent, string* _id)
+{
+	//pointer
+	ID = _id;
+	parent = _parent;
+
+	//member
+	threadManager = new ThreadManager(this);
+}
