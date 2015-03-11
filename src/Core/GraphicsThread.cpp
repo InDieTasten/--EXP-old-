911a708e859c9f52afc68d7c8718c3456e43023e
InDieTasten <indietasten@gmail.com>
2015-03-11 21:44:43 +0100

    GraphicsThread source constructor
    Former-commit-id: 1404c24f696540c8d3d6ceaf06d5316b59db824b [formerly 91713bc562dd06e3b980055baa8701540c78375b]
    Former-commit-id: 90fb2897cbfeb0d25dfb2e0c84646bd7c7edabdb

--- src/Core/GraphicsThread.cpp
+++ src/Core/GraphicsThread.cpp
@@ -1,1 +1,9 @@
 #include <Core\GraphicsThread.hpp>
+
+GraphicsThread::GraphicsThread(ThreadManager* _parent)
+{
+	//pointer
+	parent = _parent;
+
+	ug::log("A GraphicsThread has been constructed");
+}
