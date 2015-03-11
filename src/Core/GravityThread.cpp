83241df7a7af2b2b09ef9198d0d8a12f217489ae
InDieTasten <indietasten@gmail.com>
2015-03-11 21:44:20 +0100

    GravityThread source ctor update
    Former-commit-id: aa2bdae609dbd7878c43f1fd22f99c00cfd702e5

--- src/Core/GravityThread.cpp
+++ src/Core/GravityThread.cpp
@@ -1,10 +1,11 @@
 #include <Core\GravityThread.hpp>
 #include <Core\ThreadManager.hpp>
+#include <Level\SolarSystem.hpp>
 
 GravityThread::GravityThread(ThreadManager* _parent)
 {
 	//pointer
 	parent = _parent;
 
-	ug::log("A GravityThread has been constructed: " + *parent->getParent()->getID);
+	ug::log("A GravityThread has been constructed: " + *parent->getParent()->getID());
 }
