8cf73331ea7d56174f1ff0aaef1ea07659368fdf
InDieTasten <indietasten@gmail.com>
2015-03-11 21:05:52 +0100

    SolarSystem source destructor
    Former-commit-id: fca496a1e71cc478cf04d15c181eb211d0ae51e7 [formerly 8b9041aa6815a728d4dac4b3ee8f1a08504778dd]
    Former-commit-id: 4f2aa8fcc6937f95f02f62e3d8315eb6f4dcc289

--- src/Level/SolarSystem.cpp
+++ src/Level/SolarSystem.cpp
@@ -9,3 +9,13 @@ SolarSystem::SolarSystem(Level* _parent, string* _id)
 	//member
 	threadManager = new ThreadManager(this);
 }
+SolarSystem::~SolarSystem()
+{
+	//pointer
+	ID = nullptr;
+	parent = nullptr;
+
+	//member
+	delete threadManager;
+	threadManager = nullptr;
+}
