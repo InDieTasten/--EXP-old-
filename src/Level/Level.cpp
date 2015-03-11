3faa5285e132a422f65a2fce01f09179e46e2a9e
InDieTasten <indietasten@gmail.com>
2015-03-11 20:29:08 +0100

    Level source ctor/dtor update
    Former-commit-id: 7801c33fc59e13c23d59a0a2264a6418f9436839

--- src/Level/Level.cpp
+++ src/Level/Level.cpp
@@ -4,9 +4,15 @@ Level::Level(Databank* _parent)
 {
 	//pointer
 	parent = _parent;
+
+	//default level
+
+
+	ug::log("A level has been constructed");
 }
 Level::~Level()
 {
 	//pointer
 	parent = nullptr;
+	ug::log("A level has been destructed");
 }
