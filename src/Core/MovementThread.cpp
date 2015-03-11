8fd7abac53c186a9257605db0e7b26c7f46bcd36
InDieTasten <indietasten@gmail.com>
2015-03-11 21:48:19 +0100

    MovementThread source constructor
    Former-commit-id: 95c5a4b932ff01c7ae88477a02f626f5824c4ede

--- src/Core/MovementThread.cpp
+++ src/Core/MovementThread.cpp
@@ -1,1 +1,11 @@
 #include <Core\MovementThread.hpp>
+#include <Core\ThreadManager.hpp>
+#include <Level\SolarSystem.hpp>
+
+MovementThread::MovementThread(ThreadManager* _parent)
+{
+	//pointer
+	parent = _parent;
+
+	ug::log("A MovementThread has been constructed: " + *parent->getParent()->getID());
+}
