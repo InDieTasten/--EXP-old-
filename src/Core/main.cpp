680e1b60cd2e3e1086c41fb261f39980feb039a1
InDieTasten <indietasten@gmail.com>
2015-03-12 22:54:46 +0100

    main source update
    Former-commit-id: 16a7c42f3d9d73b623e1bb6cf18573245b1d719a [formerly 2685c7078fb285b15206393f4f014f1f015ae39a]
    Former-commit-id: 15ad46c69603f3c3fa64f8ddc996180a35d7a9f7

--- src/Core/main.cpp
+++ src/Core/main.cpp
@@ -23,13 +23,14 @@ int main(int argc, char *argv[])
 {
 	ug::log("[Info]Game is launching in version: " + VERSION::version);
 	sf::RenderWindow App(sf::VideoMode(1280, 720, 32), VERSION::name + " " + VERSION::version, sf::Style::Titlebar | sf::Style::Resize | sf::Style::Close);
-	
+	App.setActive(false);
+
 	EventThread eventThread(&App);
 
 	Databank* databank = new Databank(&App, &eventThread);
 
 	databank->getLevel()->addSystem(new string("test"));
-	databank->getLevel()->removeSystem(new string("test"));
+	databank->getLevel()->getSystem("test")->getThreadManager()->launch();
 
 	eventThread.run();
 
