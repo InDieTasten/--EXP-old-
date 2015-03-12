5fbd598c72d701faffd301984a2f9f8fe8017e45
InDieTasten <indietasten@gmail.com>
2015-03-12 22:12:31 +0100

    GraphicsThread source run
    Former-commit-id: 5db448ffa90920a9f9395e142bd83e5dc1feddfd

--- src/Core/GraphicsThread.cpp
+++ src/Core/GraphicsThread.cpp
@@ -5,6 +5,9 @@ GraphicsThread::GraphicsThread(ThreadManager* _parent)
 	//pointer
 	parent = _parent;
 
+	running = false;
+	looptime = chrono::milliseconds(1000/40);
+
 	ug::log("[Info]A GraphicsThread has been constructed");
 }
 GraphicsThread::~GraphicsThread()
@@ -16,5 +19,19 @@ GraphicsThread::~GraphicsThread()
 }
 void GraphicsThread::launch()
 {
+	running = true;
 	me = thread(&GraphicsThread::run, this);
+}
+void GraphicsThread::run()
+{
+	ug::log("[Info]GraphicsThread is running in this thread");
+	chrono::steady_clock::time_point last = chrono::steady_clock::now();
+	while (running)
+	{
+		//Thread throttle
+		this_thread::sleep_for(looptime - (chrono::steady_clock::now() - last));
+		last = chrono::steady_clock::now();
+
+	}
+	ug::log("[Info]GraphicsThread has stopped running in this thread");
 }
