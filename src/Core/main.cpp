6f0c95cadf2036b0709ac447feb0c9a2fbe6c563
InDieTasten <indietasten@gmail.com>
2015-03-14 00:03:29 +0100

    main source game startup
    Former-commit-id: 1cab81822508c4456787a432b696096f68fc1059

--- src/Core/main.cpp
+++ src/Core/main.cpp
@@ -31,8 +31,22 @@ int main(int argc, char *argv[])
 	Databank* databank = new Databank(&eventThread, &graphThread);
 
 	databank->getLevel()->addSystem(new string("test"));
-	databank->getLevel()->getSystem("test")->activate();
 
+	SolarSystem* working = databank->getLevel()->getSystem("test");
+
+	//Tell solar system to construct it's thread manager
+	working->activate();
+
+	//Tell thread manager to launch itself
+	working->getThreadManager()->launch();
+
+	//Tell thread manager to start rendering
+	working->getThreadManager()->startRender(App.getDefaultView());
+
+	//Tell thread manager to start simulating
+	working->getThreadManager()->startSimulation();
+
+	//Enter event loop
 	eventThread.run();
 
 	ug::log("[Info]Game quit!");
