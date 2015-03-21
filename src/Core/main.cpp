81639552946c96d2c2373f7d55ea0010066b5c74
InDieTasten <indietasten@gmail.com>
2015-03-21 16:10:30 +0100

    Testing environment
    Former-commit-id: af0bb631aa967e7280ef951f5c7037fce984247b

--- src/Core/main.cpp
+++ src/Core/main.cpp
@@ -30,48 +30,8 @@ int main(int argc, char *argv[])
 {
 	ug::log("[Info]Game is launching in version: " + VERSION::version);
 	sf::RenderWindow App(sf::VideoMode(1280, 720, 32), VERSION::name + " " + VERSION::version, sf::Style::Titlebar | sf::Style::Resize | sf::Style::Close);
-	App.setActive(false);
-	ug::renderwindow = &App;
 
-	EventThread eventThread(&App);
-	ug::eventthread = &eventThread;
-	GraphicsThread graphThread(&App);
-	ug::graphicsthread = &graphThread;
 
-	Databank* databank = new Databank(&eventThread, &graphThread);
-	ug::databank = databank;
-
-	databank->getLevel()->addSystem(new string("test"));
-
-	SolarSystem* working = databank->getLevel()->getSystem("test");
-
-	//Tell solar system to construct it's thread manager
-	working->activate();
-
-	//Tell thread manager to launch itself
-	working->getThreadManager()->launch();
-	working->getThreadManager()->terminate();
-
-	working->deactivate();
-	//Tell thread manager to start rendering
-	//working->getThreadManager()->startRender(App.getDefaultView());
-
-	//Tell thread manager to start simulating
-	//working->getThreadManager()->startSimulation();
-
-	//Enter event loop
-	eventThread.run();
-
-	//Terminate game
-	ug::log("[KILL]Entering TERMINATION sequence");
-	ug::log("[KILL]Destroying Databank environment");
-	delete ug::databank;
-	ug::log("[KILL]Destroying GraphicsThread environment");
-	delete ug::graphicsthread;
-	ug::log("[KILL]Destroying EventThread environment");
-	delete ug::eventthread;
-	ug::log("[KILL]Closing RenderWindow");
-	ug::renderwindow->close();
 
 	ug::log("[Info]Game quit!");
 	getchar();
