4e817db936c85b94b4a09df20e3415550b2ad0e9
InDieTasten <indietasten@gmail.com>
2015-04-04 05:53:49 +0200

    sfml threading
    Former-commit-id: c527c12f2359928d0161e1d3450c8bdc31e37824 [formerly 0f46dc445174b1c292e08508b6a417f16b15f26e]
    Former-commit-id: a9cdb99f84a84d6403c67d292c09d590a3959fb2

--- src/Core/main.cpp
+++ src/Core/main.cpp
@@ -5,7 +5,7 @@
 
 #include <mutex>
 
-extern std::mutex loggermtx;
+extern sf::Mutex loggermtx;
 extern void EXP::log(std::string);
 
 extern "C" {
@@ -16,12 +16,16 @@ extern "C" {
 
 #include <Assets\TextureManager.hpp>
 
+
 int main(int argc, char *argv[])
 {
 	EXP::log("[Info]Game is launching in version: " + VERSION::version);
 	sf::RenderWindow Window(sf::VideoMode(1280, 720, 32), VERSION::name + " " + VERSION::version, sf::Style::Titlebar | sf::Style::Resize | sf::Style::Close);
 
+	sf::Thread thrd(&EXP::log, "[Test]");
+	thrd.launch();
 
+	sf::sleep(sf::seconds(1));
 
 	Window.close();
 	EXP::log("[Info]Game quit!");
