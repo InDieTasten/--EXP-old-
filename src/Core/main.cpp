c259801fb1262b5ef0ca18d7a36012d1b35daa05
InDieTasten <indietasten@gmail.com>
2015-04-04 06:01:42 +0200

    -
    Former-commit-id: 8298f65a248a0557ea9cbf692e83791827e05f95

--- src/Core/main.cpp
+++ src/Core/main.cpp
@@ -14,18 +14,13 @@ extern "C" {
 #include <lauxlib.h>
 }
 
-#include <Assets\TextureManager.hpp>
-
 
 int main(int argc, char *argv[])
 {
 	EXP::log("[Info]Game is launching in version: " + VERSION::version);
 	sf::RenderWindow Window(sf::VideoMode(1280, 720, 32), VERSION::name + " " + VERSION::version, sf::Style::Titlebar | sf::Style::Resize | sf::Style::Close);
 
-	sf::Thread thrd(&EXP::log, "[Test]");
-	thrd.launch();
-
-	sf::sleep(sf::seconds(1));
+	
 
 	Window.close();
 	EXP::log("[Info]Game quit!");
