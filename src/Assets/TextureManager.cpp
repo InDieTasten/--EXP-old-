ac19686680794ec344b4dc375b9174bd4dd8d639
InDieTasten <indietasten@gmail.com>
2015-04-04 04:58:02 +0200

    TextureManager class

---
+++ src/Assets/TextureManager.cpp
@@ -0,0 +1,47 @@
+#include <Assets\TextureManager.hpp>
+
+TextureManager::TextureManager()
+{
+	EXP::log("[Info]TextureManager has been constructed");
+}
+TextureManager::~TextureManager()
+{
+	if (textures.size() != 0)
+	{
+		EXP::log("[Warning]Forcefully destructing textures");
+		for (auto it : textures)
+		{
+			delete it.second;
+			it.second = nullptr;
+		}
+	}
+	EXP::log("[Info]TextureManager has been destructed");
+}
+void TextureManager::addTexture(std::string _id, std::string _path)
+{
+	if (textures.count(_id))
+	{
+		EXP::log("[Error]Tried registrating already registered Texture: " + _id);
+		return;
+	}
+	textures[_id] = nullptr;
+	const std::string* ptr = &textures.find(_id)->first;
+	textures[*ptr] = new Texture(ptr, _path);
+	EXP::log("[Info]Texture has been added to Manager: " + _id);
+}
+void TextureManager::removeTexture(std::string _id)
+{
+	textures.erase(textures.find(_id));
+	EXP::log("[Info]Texture has been removed from Manager: " + _id);
+}
+Texture* TextureManager::getTexture(std::string _id)
+{
+	Texture* tmp = textures[_id];
+	if (tmp)
+	{
+		return tmp;
+	}
+	else {
+		EXP::log("[Warning]Trtied to get unknown Texture: " + _id);
+	}
+}
