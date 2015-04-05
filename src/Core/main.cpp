e6453d2920f69ed65b895b08d3874267fb684de4
InDieTasten <indietasten@gmail.com>
2015-04-05 11:53:58 +0200

    -
    Former-commit-id: e16efaa9c8af7eb5851a6b196335dbdf6d95790b [formerly 6fede7a0d32fe5cedcd1e0cd62f166ef102f0c31]
    Former-commit-id: e3c157b39276f5b98ff8514a43eba2a85d6ac473

--- src/Core/main.cpp
+++ src/Core/main.cpp
@@ -2,6 +2,7 @@
 #include <Utilities\VERSION.hpp>
 #include <Utilities\Logger.hpp>
 #include <SFML\Graphics.hpp>
+#include <Input\EventManager.hpp>
 
 extern sf::Mutex loggermtx;
 extern void EXP::log(std::string);
@@ -16,11 +17,10 @@ extern "C" {
 }
 
 
-
-#include <Input\EventManager.hpp>
 namespace EXP {
-	EventManager* eventManager;
+	EventManager* eventManager = nullptr;
 }
+
 void gameQuit(sf::Event::KeyEvent _event)
 {
 	if (_event.code == sf::Keyboard::Escape)
@@ -35,13 +35,13 @@ int main(int argc, char *argv[])
 	EXP::log("[Info]Game is launching in version: " + VERSION::version);
 	sf::RenderWindow Window(sf::VideoMode(1280, 720, 32), VERSION::name + " " + VERSION::version, sf::Style::Titlebar | sf::Style::Resize | sf::Style::Close);
 
-	EventManager man(&Window);
-	EXP::eventManager = &man;
+	EXP::eventManager = new EventManager(&Window);
+	EXP::eventManager->addKeyRelease(&gameQuit);
 
-	man.addKeyPress(&gameQuit);
+	EXP::eventManager->listen();
 
-	man.listen();
-
+	delete EXP::eventManager;
+	EXP::eventManager = nullptr;
 	Window.close();
 	EXP::log("[Info]Game quit!");
 	return EXIT_SUCCESS;
