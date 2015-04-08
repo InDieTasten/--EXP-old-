e7fd090bb34fe91bb05d2fb67b51e29e56491b08
InDieTasten <indietasten@gmail.com>
2015-04-08 04:21:06 +0200

    Mesh class
    Former-commit-id: f2ad948a131951a7a666dbd9090e7dc437d56118 [formerly 6f97b6b059b18a9e9b8eb8f80456f94e0f1467ba]
    Former-commit-id: e3549382d13b4454f5599699993418eafa4554a6

--- include/Data/Mesh.hpp
+++ include/Data/Mesh.hpp
@@ -7,55 +7,10 @@
 
 class Mesh
 {
-public:
-	class Vertex
-	{
-	public:
-		float x;
-		float y;
-		Vertex();
-		Vertex(float _x, float _y);
-		~Vertex();
-
-		Vertex& operator+=(const Vertex& rhs);
-		Vertex& operator-=(const Vertex& rhs);
-		Vertex& operator*=(const Vertex& rhs);
-		Vertex& operator/=(const Vertex& rhs);
-		Vertex& operator+(const Vertex& rhs);
-		Vertex& operator-(const Vertex& rhs);
-		Vertex& operator*(const Vertex& rhs);
-		Vertex& operator/(const Vertex& rhs);
-		bool operator==(const Vertex& rhs);
-		bool operator!=(const Vertex& rhs);
-
-		void draw(sf::RenderTarget* _target, sf::Color _color);
-	};
-	class Strip
-	{
-	private:
-		std::vector<Mesh::Vertex> vertices;
-		bool isArea();
-		bool isStripe();
-		bool isPoint();
-	public:
-		Strip();
-		~Strip();
-
-		void addVertex(Vertex _vertex);
-		void addVertex(int _pos, Vertex _vertex);
-		void removeVertex(int _pos);
-		void setVertex(int _pos, Vertex _vertex);
-		Vertex getVertex(int _pos);
-		void setVertices(std::vector<Vertex> _vertices);
-		std::vector<Vertex> getVertices();
-
-		float getArea();
-		Vertex getAreaCenter();
-
-		void draw(sf::RenderTarget* _target, sf::Color _color);
-	};
 private:
-	std::vector<Strip> strips;
+	std::vector<sf::Vertex> vertices;
+	int orientation(sf::Vector2f _a, sf::Vector2f _b, sf::Vector2f _c);
+	bool isValid();
 public:
 	
 
@@ -63,24 +18,20 @@ public:
 	~Mesh();
 
 	void clear();
-	void addStrip(Strip _strip);
-	void addStrip(int _pos, Strip _strip);
-	void removeStrip(int _pos);
-	void setStrip(int _pos, Strip _strip);
-	Strip getStrip(int _pos);
-	void setStrips(std::vector<Strip> _strips);
-	std::vector<Strip> getStrips();
+	void addVertex(sf::Vertex _vertex);
+	void addVertex(int _pos, sf::Vertex _vertex);
+	void removeVertex(int _pos);
+
+	void setVertex(int _pos, sf::Vertex _vertex);
+	sf::Vertex getVertex(int _pos);
+
+	void setVertices(std::vector<sf::Vertex> _vertices);
+	std::vector<sf::Vertex> getVertices();
 
 	bool checkOverlapping(Mesh _other); //
-	Mesh getOverlapping(Mesh _other); //
 
 	float getArea(); //
-	Vertex getAreaCenter(); //
-
-	Mesh& operator+=(const Mesh& rhs); //
-	Mesh& operator-=(const Mesh& rhs); //
-	Mesh& operator+(const Mesh& rhs); //
-	Mesh& operator-(const Mesh& rhs); //
+	sf::Vector2f getAreaCenter(); //
 
 	void draw(sf::RenderTarget* _target, sf::Color _color);
 };
