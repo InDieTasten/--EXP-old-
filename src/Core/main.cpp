79f5bb797bf3a4e0a0f23e4d13ab2bd34e4e3a24
InDieTasten <indietasten@gmail.com>
2015-05-01 12:41:15 +0200

    Testzone clear
    Former-commit-id: 53ac5c1c82d4a19811d60def3c539aa47afbf384

--- src/Core/main.cpp
+++ src/Core/main.cpp
@@ -9,7 +9,7 @@ extern void EXP::log(std::string);
 extern void EXP::init();
 
 //TESTZONE START
-#include <Data\Mesh.hpp>
+
 //TESTZONE END
 
 
@@ -45,51 +45,9 @@ int main(int argc, char *argv[])
 	EXP::eventManager->addKeyRelease(&gameQuit);
 
 	//TESTZONE START
-	Mesh* test1 = new Mesh();
-
-	test1->addVertex(sf::Vertex(sf::Vector2f(-20, 20)));
-	test1->addVertex(sf::Vertex(sf::Vector2f(20, 20)));
-	test1->addVertex(sf::Vertex(sf::Vector2f(20, -20)));
-	test1->addVertex(sf::Vertex(sf::Vector2f(-10, -10)));
-	test1->addVertex(sf::Vertex(sf::Vector2f(5, 5)));
 	
-	Mesh* test2 = new Mesh();
 
-	test2->addVertex(sf::Vertex(sf::Vector2f(-10, 10)));
-	test2->addVertex(sf::Vertex(sf::Vector2f(30, 10)));
-	test2->addVertex(sf::Vertex(sf::Vector2f(30, -30)));
-	test2->addVertex(sf::Vertex(sf::Vector2f(0, -20)));
-	test2->addVertex(sf::Vertex(sf::Vector2f(15, 0)));
 
-	std::list<sf::Vector2f> coll = test1->overlap(sf::Transform(), test2, sf::Transform().translate(5, 0));
-	Window.clear();
-	for (auto point : coll)
-	{
-		EXP::log("[Debug]Render circle: " + utils::tostring(point.x) + utils::tostring(point.y));
-		sf::CircleShape shape(4.0/8.0);
-		shape.setOutlineColor(sf::Color::Red);
-		shape.setFillColor(sf::Color::Transparent);
-		shape.setOutlineThickness(1.0/8.0);
-		shape.setPosition(point);
-		shape.setOrigin(4.0 / 8.0, 4.0 / 8.0);
-		sf::Transform pos;
-		pos.scale(8.0, 8.0);
-		Window.draw(shape, pos);
-	}
-
-	
-	sf::Transformable x;
-	x.setScale(8.0,8.0);
-	
-	test1->draw(&Window, x, sf::Color(0, 255, 0, 32));
-	x.move(5*8, 0);
-	test2->draw(&Window, x, sf::Color(0, 0, 255, 32));
-	Window.display();
-
-	delete test1;
-	test1 = nullptr;
-	delete test2;
-	test2 = nullptr;
 	//TESTZONE END
 
 	EXP::eventManager->listen();
