778b31cc33e5ba2de6e3642275e762e387390408
InDieTasten <indietasten@gmail.com>
2015-05-05 06:05:12 +0200

    ResourceType class
    Former-commit-id: f80e7da613c36c521a1679f9bfd578e2f91bb951 [formerly b1dbffe3953793cee1cfbcc1d0859328d3c60fd5]
    Former-commit-id: 3871813c10a8578dd3afb966b40836961d869cc3

---
+++ include/Data/ResourceType.hpp
@@ -0,0 +1,16 @@
+#ifndef _ResourceType_hpp_
+#define _ResourceType_hpp_
+
+#include <string>
+
+class ResourceType
+{
+private:
+	std::string name;
+	std::string unit;
+public:
+	ResourceType();
+	~ResourceType();
+};
+
+#endif // !_ResourceType_hpp_
