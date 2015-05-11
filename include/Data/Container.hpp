06c1d10da661c1db717497d4dfa24c43a3afa2e6
InDieTasten <indietasten@gmail.com>
2015-05-12 00:15:26 +0200

    Container class init
    Former-commit-id: 8139a006bcf0d01d122b864791258d66f7104bb0

---
+++ include/Data/Container.hpp
@@ -0,0 +1,14 @@
+#ifndef _Container_hpp_
+#define _Container_hpp_
+
+#include <Utilities\Logger.hpp>
+
+class Container
+{
+private:
+public:
+	Container();
+	~Container();
+};
+
+#endif // !_Container_hpp_
---
+++ src/Data/Container.cpp
@@ -0,0 +1,2 @@
+#include <Data\Container.hpp>
+
