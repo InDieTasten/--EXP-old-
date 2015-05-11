5a4040784d3e70126a5d604ad47b03d21a35cc53
InDieTasten <indietasten@gmail.com>
2015-05-11 22:31:08 +0200

    ResourceType header changes
    Former-commit-id: f1440239b2476e70f4eaaf74c7c8b611ac597644 [formerly ff65381cba70a268f51e139cb68db34ddf6a548c]
    Former-commit-id: 81700e0bde654cbb573e1c36addb83a036a7de8d

--- include/Data/ResourceType.hpp
+++ include/Data/ResourceType.hpp
@@ -11,6 +11,11 @@ private:
 public:
 	ResourceType();
 	~ResourceType();
+
+	void setName(std::string _name);
+	void setUnit(std::string _unit);
+	std::string getName();
+	std::string getUnit();
 };
 
 #endif // !_ResourceType_hpp_
