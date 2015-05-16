795cffde4becfd295620bec66e48262945cdd2b1
InDieTasten <indietasten@gmail.com>
2015-05-16 12:19:30 +0200

    Container class header Inheritance
    Former-commit-id: 15be4956e7f0ae4508979f49ca4826b35c1f9071 [formerly 3e8b20e76a28a21845be7b0cfe372ff0d7a7c8a5]
    Former-commit-id: 070e004cd6a14eab4bd99137b57475fed79d4ecb

--- include/Data/Container.hpp
+++ include/Data/Container.hpp
@@ -5,12 +5,18 @@
 #include <Data\Taggable.hpp>
 #include <Utilities\Logger.hpp>
 
+#include <Data\ResourceType.hpp>
+#include <map>
+
 class Container : public Attachable, public Taggable
 {
 private:
+	std::map<ResourceType, float> resources;
 public:
 	Container();
 	~Container();
+
+
 };
 
 #endif // !_Container_hpp_
