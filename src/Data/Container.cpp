752c1bdbfa05665cb0a2397962f2335fa0032d10
InDieTasten <indietasten@gmail.com>
2015-05-16 23:28:07 +0200

    Container source update
    Former-commit-id: 1ad05e245270bd393b3249d056c895269c83cb18

--- src/Data/Container.cpp
+++ src/Data/Container.cpp
@@ -1,2 +1,10 @@
 #include <Data\Container.hpp>
 
+Container::Container() : Attachable(), Taggable()
+{
+	EXP::log("[Info]Container has been constructed: " + utils::tostring(this));
+}
+Container::~Container()
+{
+	EXP::log("[Info]Container has been destrcuted: " + utils::tostring(this));
+}
