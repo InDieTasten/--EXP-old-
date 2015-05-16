bbb2ed50df6b61f16b34303b60c08e566e5ebf7c
InDieTasten <indietasten@gmail.com>
2015-05-16 23:28:29 +0200

    ResourceType class source update
    Former-commit-id: 06d4c00a45fac6a8b739dde29c02ca1932601288 [formerly cb4b5b6bba265091bff7dfc0eff9fec4661777c1]
    Former-commit-id: e8bb7740d033db8f225c719b6f1d8a8b0802b945

--- src/Data/ResourceType.cpp
+++ src/Data/ResourceType.cpp
@@ -1,2 +1,10 @@
 #include <Data\ResourceType.hpp>
 
+ResourceType::ResourceType() : Taggable()
+{
+	EXP::log("[Info]ResourceType has been constructed: " + utils::tostring(this));
+}
+ResourceType::~ResourceType()
+{
+	EXP::log("[Info]ResourceType has been destructed: " + utils::tostring(this));
+}
