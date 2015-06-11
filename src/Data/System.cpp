febfe16f94a3d415d30a48d19d1c154431bde4ff
InDieTasten <indietasten@gmail.com>
2015-06-11 03:16:48 +0200

    System class implementation
    Former-commit-id: de88649d5c1b9ec062ffe5d36e18c9bc3f809ce6

--- src/Data/System.cpp
+++ src/Data/System.cpp
@@ -8,3 +8,28 @@ System::~System()
 {
 	EXP::log("[Info]System has been destrcted: " + utils::tostring(this));
 }
+
+void System::addObject(PhysicalInfo* _object)
+{
+	objects.push_back(_object);
+	EXP::log("[Info]Object has been added to System: " + utils::tostring(this));
+}
+void System::removeObject(PhysicalInfo* _object)
+{
+	objects.remove(_object);
+	EXP::log("[Info]Object has been removed from System: " + utils::tostring(this));
+}
+void System::setObjects(std::list<PhysicalInfo*> _objects)
+{
+	objects = _objects;
+	EXP::log("[Info]Systems objects have been overriden: " + utils::tostring(this));
+}
+
+std::list<PhysicalInfo*> System::getObjects()
+{
+	return objects;
+}
+std::list<PhysicalInfo*>* System::getObjectPointers()
+{
+	return &objects;
+}
