ff2ba42e1aa102e648e9fd13d11ca2b47b0526df
InDieTasten <indietasten@gmail.com>
2015-06-14 10:28:47 +0200

    GameEngine class constructor and destructor
    Former-commit-id: 29a9a18b9d46c375d25964e9f7386eb2241b9532

--- src/Processing/GameEngine.cpp
+++ src/Processing/GameEngine.cpp
@@ -1,3 +1,12 @@
 #include <Processing\GameEngine.hpp>
 
+GameEngine::GameEngine()
+{
 
+	EXP::log("[Info]GameEngine has been  constructed: " + utils::tostring(this));
+}
+GameEngine::~GameEngine()
+{
+
+	EXP::log("[Info]GameEngine has been  destructed: " + utils::tostring(this));
+}
