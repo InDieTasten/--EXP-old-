71e788656be91c1393a17601b8c42fd47b099291
InDieTasten <indietasten@gmail.com>
2015-06-14 10:26:02 +0200

    Renderer class constructor and destrcutor
    Former-commit-id: 3dc8597985f49ebbd8702531e33c94dafc6a5dcb

--- src/Processing/Renderer.cpp
+++ src/Processing/Renderer.cpp
@@ -1,2 +1,12 @@
 #include <Processing\Renderer.hpp>
 
+Renderer::Renderer()
+{
+
+	EXP::log("[Info]Renderer has been  constructed: " + utils::tostring(this));
+}
+Renderer::~Renderer()
+{
+
+	EXP::log("[Info]Renderer has been  destructed: " + utils::tostring(this));
+}
