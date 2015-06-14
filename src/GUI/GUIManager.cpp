f1aa773816254b8faf22a7b8df674709b59040f1
InDieTasten <indietasten@gmail.com>
2015-06-14 14:24:56 +0200

    GUIManager class init
    Former-commit-id: bd1da2c6aa90ac33cf4531718a15d84a4be6f38a [formerly e07df6055d3cac3a75a36dcb0cf9bca27fcddd84]
    Former-commit-id: 27fb9c415f13535fe4518e36e7c9e4e1ce22a2e9

---
+++ src/GUI/GUIManager.cpp
@@ -0,0 +1,39 @@
+#include <GUI\GUIManager.hpp>
+
+GUIManager::GUIManager()
+{
+	EXP::log("[Info]GUIManager has been constructed: " + utils::tostring(this));
+}
+GUIManager::~GUIManager()
+{
+	EXP::log("[Info]GUIManager has been destructed: " + utils::tostring(this));
+}
+
+int GUIManager::addMenu(GUIMenu& _menu)
+{
+	menus.push_back(_menu);
+	return menus.size() - 1;
+}
+GUIMenu& GUIManager::getMenu(int _pos)
+{
+	return menus[_pos];
+}
+void GUIManager::removeMenu(int _pos)
+{
+	menus.erase(menus.begin() + _pos);
+}
+
+void GUIManager::draw(sf::RenderTarget& _target, sf::RenderStates _states)
+{
+	for (auto it : menus)
+	{
+		it.draw(_target, _states);
+	}
+}
+void GUIManager::handleEvent(sf::Event* _event)
+{
+	for (auto it : menus)
+	{
+		it.handleEvent(_event);
+	}
+}
