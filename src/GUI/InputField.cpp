7801606aeb93a1ad946087f69ea41262d7a4f899
InDieTasten <indietasten@gmail.com>
2015-06-14 14:10:57 +0200

    InputField class init

---
+++ src/GUI/InputField.cpp
@@ -0,0 +1,45 @@
+#include <GUI\InputField.hpp>
+
+InputField::InputField()
+{
+	type = ElementType::InputField;
+	EXP::log("[Info]InputField has been constructed: " + utils::tostring(this));
+}
+InputField::~InputField()
+{
+	EXP::log("[Info]InputField has been destructed: " + utils::tostring(this));
+}
+
+void InputField::draw(sf::RenderTarget* target, sf::RenderStates states)
+{
+	//WORK
+}
+void InputField::handleEvent(sf::Event* _event)
+{
+	//WORK
+}
+
+void InputField::setWidth(int _width)
+{
+	width = _width;
+}
+void InputField::setHeight(int _height)
+{
+	height = _height;
+}
+void InputField::setContent(std::string _content)
+{
+	content = _content;
+}
+int InputField::getWidth()
+{
+	return width;
+}
+int InputField::getHeight()
+{
+	return height;
+}
+std::string InputField::getContent()
+{
+	return content;
+}
