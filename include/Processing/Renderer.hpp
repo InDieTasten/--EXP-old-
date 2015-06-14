21a88e9744b5185dff1026a0789c63d81c199cbf
InDieTasten <indietasten@gmail.com>
2015-06-14 10:51:26 +0200

    Renderer class header update
    Former-commit-id: b5e8488490ec106cec612d1c1d06fbfdb7dc5bac [formerly b2d65b20b380ce416e04bf3d5ab52877fafd3c65]
    Former-commit-id: 86a4fc8af22e60a256a5c655f453f9cfcacd0999

--- include/Processing/Renderer.hpp
+++ include/Processing/Renderer.hpp
@@ -7,11 +7,24 @@
 class Renderer
 {
 private:
+	bool running;
+
+	sf::RenderWindow* window;
+	System* level;
+
+	void run(); //internal method for thread
+
 public:
-	Renderer();
+	Renderer(sf::RenderWindow* _window, System* _level);
 	~Renderer();
 
+	void launch();
+	void terminate();
 
+	void setWindow(sf::RenderWindow* _window);
+	void setLevel(System* _level);
+	sf::RenderWindow* getWindow();
+	System* getLevel();
 };
 
 #endif // !_Renderer_hpp_
