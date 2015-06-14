c82da8769c916b0f4782227ae52d5f308b230852
InDieTasten <indietasten@gmail.com>
2015-06-14 13:34:57 +0200

    Responsice class init
    Former-commit-id: 9448aa005c51b13a65072c2f926bc60e1a55aff7

---
+++ src/GUI/Responsive.cpp
@@ -0,0 +1,12 @@
+#include <GUI\Responsive.hpp>
+
+Responsive::Responsive()
+{
+
+	EXP::log("[Info]Responsive has been constructed: " + utils::tostring(this));
+}
+Responsive::~Responsive()
+{
+
+	EXP::log("[Info]Responsive has been destructed: " + utils::tostring(this));
+}
