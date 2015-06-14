e5e6e16bcdc594699b6f18a05432f23215b1db55
InDieTasten <indietasten@gmail.com>
2015-06-14 10:26:51 +0200

    Simulator class constructor and destructor

--- src/Processing/Simulator.cpp
+++ src/Processing/Simulator.cpp
@@ -1,3 +1,12 @@
 #include <Processing\Simulator.hpp>
 
+Simulator::Simulator()
+{
 
+	EXP::log("[Info]Simulator has been  constructed: " + utils::tostring(this));
+}
+Simulator::~Simulator()
+{
+
+	EXP::log("[Info]Simulator has been  destructed: " + utils::tostring(this));
+}
