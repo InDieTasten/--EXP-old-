68bfd9bcbac837b052fd8d47ac68583cca910157
InDieTasten <indietasten@gmail.com>
2015-06-14 10:38:54 +0200

    GameEngine class launch and terminate additions

--- include/Processing/GameEngine.hpp
+++ include/Processing/GameEngine.hpp
@@ -1,15 +1,32 @@
 #ifndef _GameEngine_hpp_
 #define _GameEngine_hpp_
 
+#include <Utilities\VERSION.hpp>
+
+#include <SFML\Graphics.hpp>
+#include <Data\System.hpp>
+#include <Input\EventManager.hpp>
+#include <Processing\Renderer.hpp>
+#include <Processing\Simulator.hpp>
+
 #include <Utilities\Conversion.hpp>
 #include <Utilities\Logger.hpp>
 
 class GameEngine
 {
 private:
+	sf::RenderWindow* gameWindow;
+	EventManager* eventManager;
+	Renderer* renderer;
+	Simulator* simulator;
+
+	System* level;
+
 public:
 	GameEngine();
 	~GameEngine();
+
+	void launch();
 };
 
 #endif // !_GameEngine_hpp_
--- src/Processing/GameEngine.cpp
+++ src/Processing/GameEngine.cpp
@@ -2,11 +2,38 @@
 
 GameEngine::GameEngine()
 {
+	gameWindow = nullptr;
+	eventManager = nullptr;
+	renderer = nullptr;
+	simulator = nullptr;
+	level = nullptr;
 
 	EXP::log("[Info]GameEngine has been  constructed: " + utils::tostring(this));
 }
 GameEngine::~GameEngine()
 {
+	//terminate all threads (WORK)
+
+	//De-allocate game memory (note, that this is quite the hardcore way to quit it)
+	delete gameWindow;
+	gameWindow = nullptr;
+	delete eventManager;
+	eventManager = nullptr;
+	delete renderer;
+	renderer = nullptr;
+	delete simulator;
+	simulator = nullptr;
+	delete level;
+	level = nullptr;
 
 	EXP::log("[Info]GameEngine has been  destructed: " + utils::tostring(this));
+}
+
+void GameEngine::launch()
+{
+	//create window
+	gameWindow = new sf::RenderWindow(sf::VideoMode(1280, 720, 32), VERSION::name + " " + VERSION::version, sf::Style::Titlebar | sf::Style::Resize | sf::Style::Close);
+	gameWindow->clear();
+
+
 }
