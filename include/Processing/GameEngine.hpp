55d25bba3228fdd418e1ca0db8630d493c452a18
InDieTasten <indietasten@gmail.com>
2015-06-14 10:15:50 +0200

    GameEngine class init

---
+++ include/Processing/GameEngine.hpp
@@ -0,0 +1,13 @@
+#ifndef _GameEngine_hpp_
+#define _GameEngine_hpp_
+
+
+class GameEngine
+{
+private:
+public:
+	GameEngine();
+	~GameEngine();
+};
+
+#endif // !_GameEngine_hpp_
---
+++ src/Processing/GameEngine.cpp
@@ -0,0 +1,3 @@
+#include <Processing\GameEngine.hpp>
+
+
