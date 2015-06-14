27fb9c415f13535fe4518e36e7c9e4e1ce22a2e9
InDieTasten <indietasten@gmail.com>
2015-06-14 14:24:56 +0200

    GUIManager class init
    Former-commit-id: e07df6055d3cac3a75a36dcb0cf9bca27fcddd84

---
+++ include/GUI/GUIManager.hpp
@@ -0,0 +1,27 @@
+#ifndef _GUIManager_hpp_
+#define _GUIManager_hpp_
+
+#include <SFML\Graphics.hpp>
+#include <GUI\Responsive.hpp>
+#include <GUI\GUIMenu.hpp>
+
+#include <Utilities\Logger.hpp>
+#include <Utilities\Conversion.hpp>
+
+class GUIManager : public sf::Drawable, public Responsive
+{
+private:
+	std::vector<GUIMenu&> menus;
+public:
+	GUIManager();
+	~GUIManager();
+
+	int addMenu(GUIMenu&);
+	GUIMenu& getMenu(int);
+	void removeMenu(int);
+
+	void draw(sf::RenderTarget& _target, sf::RenderStates _states);
+	void handleEvent(sf::Event* _event);
+};
+
+#endif // !_GUIManager_hpp_
