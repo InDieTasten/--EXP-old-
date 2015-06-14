b05288b6a1c8256cbdce2e25c48b57af3c9685c7
InDieTasten <indietasten@gmail.com>
2015-06-15 00:49:41 +0200

    Font class init

---
+++ include/Assets/Font.hpp
@@ -0,0 +1,28 @@
+#ifndef _Font_hpp_
+#define _Font_hpp_
+
+
+#include <SFML\Graphics.hpp>
+#include <string>
+#include <Utilities\Logger.hpp>
+
+extern void EXP::log(std::string);
+
+class Font
+{
+private:
+	sf::Font* data;
+	const std::string* id;
+	std::string path;
+public:
+	Font(const std::string* _id, std::string _path);
+	~Font();
+
+	void load();
+	void unload();
+	sf::Font* get();
+	std::string getID();
+	std::string getPath();
+};
+
+#endif // !_Font_hpp_
