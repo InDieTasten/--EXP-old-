ad58dd61f5e18aaad668f9e7c415abbee3965357
InDieTasten <indietasten@gmail.com>
2015-06-14 11:27:44 +0200

    Refactoring

--- src/Core/main.cpp
+++ src/Core/main.cpp
@@ -3,6 +3,7 @@
 #include <Utilities\Logger.hpp>
 #include <SFML\Graphics.hpp>
 #include <Input\EventManager.hpp>
+#include <Processing\GameEngine.hpp>
 
 extern sf::Mutex loggermtx;
 extern void EXP::log(std::string);
@@ -21,35 +22,15 @@ extern "C" {
 #include <lauxlib.h>
 }
 
-
-namespace EXP {
-	EventManager* eventManager = nullptr;
-}
-void gameQuit(sf::Event::KeyEvent _event)
-{
-	if (_event.code == sf::Keyboard::Escape)
-	{
-		EXP::eventManager->terminate();
-	}
-}
 int gameStart(int argc, char *argv[])
 {
 	EXP::init();
 	EXP::log("[Info]Game is launching in version: " + VERSION::version);
-	sf::RenderWindow Window(sf::VideoMode(1280, 720, 32), VERSION::name + " " + VERSION::version, sf::Style::Titlebar | sf::Style::Resize | sf::Style::Close);
-	sf::View view = Window.getDefaultView();
-	view.setCenter(0.0f, 0.0f);
-	Window.setView(view);
-
-	EXP::eventManager = new EventManager(&Window);
-	EXP::eventManager->addKeyRelease(&gameQuit);
-
-	EXP::eventManager->listen();
-
-	delete EXP::eventManager;
-	EXP::eventManager = nullptr;
-
-	Window.close();
+	
+	GameEngine* engine = new GameEngine();
+	engine->launch();
+	delete engine;
+	engine = nullptr;
 
 	EXP::log("[Info]Game quit!");
 	return EXIT_SUCCESS;
