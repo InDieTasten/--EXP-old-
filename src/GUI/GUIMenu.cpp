f5f6887a60b16eb6806cc4880ada419227155ccd
InDieTasten <indietasten@gmail.com>
2015-06-14 13:51:08 +0200

    GUIMenu class init
    Former-commit-id: 65b878e56ca717e279342223e980615face1899b [formerly 8f9bb51336c387a98081a70e6e177d6111c27dbf]
    Former-commit-id: a6a40ac2eac58d16afe18d1f565854fd597cd64a

--- src/GUI/GUIMenu.cpp
+++ src/GUI/GUIMenu.cpp
@@ -10,3 +10,58 @@ GUIMenu::~GUIMenu()
 
 	EXP::log("[Info]GUIMenu has been destructed: " + utils::tostring(this));
 }
+
+int GUIMenu::addElement(GUIElement& _element)
+{
+	elements.push_back(_element);
+	return elements.size() - 1;
+}
+GUIElement& GUIMenu::getElement(int _pos)
+{
+	return elements[_pos];
+}
+void GUIMenu::removeElement(int _pos)
+{
+	elements.erase(elements.begin() + _pos);
+}
+
+void GUIMenu::setX(int _x)
+{
+	x = _x;
+}
+void GUIMenu::setY(int _y)
+{
+	y = _y;
+}
+void GUIMenu::setWidth(int _width)
+{
+	width = _width;
+}
+void GUIMenu::setHeight(int _height)
+{
+	height = _height;
+}
+void GUIMenu::setTitle(std::string _title)
+{
+	title = _title;
+}
+int GUIMenu::getX()
+{
+	return x;
+}
+int GUIMenu::getY()
+{
+	return y;
+}
+int GUIMenu::getWidth()
+{
+	return width;
+}
+int GUIMenu::getHeight()
+{
+	return height;
+}
+std::string GUIMenu::getTitle()
+{
+	return title;
+}
