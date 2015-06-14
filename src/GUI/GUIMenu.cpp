2c04c2ce71a82aa6893d60caccd567fc625afd47
InDieTasten <indietasten@gmail.com>
2015-06-14 13:06:28 +0200

    GUIMenu class init

---
+++ src/GUI/GUIMenu.cpp
@@ -0,0 +1,12 @@
+#include <GUI\GUIMenu.hpp>
+
+GUIMenu::GUIMenu()
+{
+
+	EXP::log("[Info]GUIMenu has been constructed: " + utils::tostring(this));
+}
+GUIMenu::~GUIMenu()
+{
+
+	EXP::log("[Info]GUIMenu has been destructed: " + utils::tostring(this));
+}
