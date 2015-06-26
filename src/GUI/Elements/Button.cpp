584e6bb03c03f80c1783b6bbc241eda6e80681bb
InDieTasten <indietasten@gmail.com>
2015-06-26 16:36:21 +0200

    Button class drawing
    Former-commit-id: ecbdbe32f80c79a867eb06a920b0ed0c16b8a990 [formerly baeea38345511a2f4f7bcdba8e0629f24aa1dd65]
    Former-commit-id: 2ff66cf037372fde1ebe2796fc99114900f942f2

--- src/GUI/Elements/Button.cpp
+++ src/GUI/Elements/Button.cpp
@@ -3,6 +3,16 @@
 Button::Button(AssetManager* _assets) : GUIElement(_assets)
 {
 	type = ElementType::Button;
+
+	width = 120;
+	height = 20;
+	text = "Undefined";
+	font = "Mono";
+	borderColor = sf::Color(0, 255, 0, 255);
+	backColor = sf::Color(80, 80, 80, 64);
+	textColor = sf::Color::White;
+	enabled = true;
+
 	EXP::log("[Info]Button has been constructed: " + utils::tostring(this));
 }
 Button::~Button()
@@ -10,9 +20,27 @@ Button::~Button()
 	EXP::log("[Info]Button has been destructed: " + utils::tostring(this));
 }
 
+void Button::update()
+{
+	components.body.setPosition(x, y);
+	components.body.setSize(sf::Vector2f(width, height));
+	components.body.setFillColor(backColor);
+	components.body.setOutlineColor(borderColor * sf::Color(255, 255, 255, 64 + enabled * (127+64)));
+	components.body.setOutlineThickness(1.0f);
+
+	components.text.setString(text);
+	components.text.setColor(textColor);
+	components.text.setFont(*assets->getFont(font)->get());
+	components.text.setCharacterSize(14);
+	components.text.findCharacterPos(1024);
+
+}
 void Button::draw(sf::RenderTarget& target, sf::RenderStates states) const
 {
-	//Button draw routine (WORK)
+	sf::FloatRect bounds = components.text.getGlobalBounds();
+	components.text.setPosition(sf::Vector2f((int)(x + width / 2 - bounds.width / 2), (int)(y-2 + height / 2 - bounds.height / 2)));
+	target.draw(components.body, states);
+	target.draw(components.text, states);
 }
 void Button::handleEvent(sf::RenderWindow& target, sf::Event* _event)
 {
@@ -22,14 +50,42 @@ void Button::handleEvent(sf::RenderWindow& target, sf::Event* _event)
 void Button::setWidth(int _width)
 {
 	width = _width;
+	update();
 }
 void Button::setHeight(int _height)
 {
 	height = _height;
+	update();
 }
 void Button::setText(std::string _text)
 {
 	text = _text;
+	update();
+}
+void Button::setFont(std::string _font)
+{
+	font = _font;
+	update();
+}
+void Button::setBorderColor(sf::Color _color)
+{
+	borderColor = _color;
+	update();
+}
+void Button::setBackColor(sf::Color _color)
+{
+	backColor = _color;
+	update();
+}
+void Button::setTextColor(sf::Color _color)
+{
+	textColor = _color;
+	update();
+}
+void Button::setEnabled(bool _enabled)
+{
+	enabled = _enabled;
+	update();
 }
 int Button::getWidth()
 {
@@ -42,4 +98,24 @@ int Button::getHeight()
 std::string Button::getText()
 {
 	return text;
+}
+std::string Button::getFont()
+{
+	return font;
+}
+sf::Color Button::getBorderColor()
+{
+	return borderColor;
+}
+sf::Color Button::getBackColor()
+{
+	return backColor;
+}
+sf::Color Button::getTextColor()
+{
+	return textColor;
+}
+bool Button::getEnabled()
+{
+	return enabled;
 }
