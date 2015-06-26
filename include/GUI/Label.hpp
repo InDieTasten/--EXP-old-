162faf8be5001f65e78363c0dfb91dbe5f214095
InDieTasten <indietasten@gmail.com>
2015-06-26 13:18:20 +0200

    Label class work
    Former-commit-id: ca2da28535a3dddf7f56251fa2c87bc6a5b29a3d

--- include/GUI/Label.hpp
+++ include/GUI/Label.hpp
@@ -2,6 +2,7 @@
 #define _Label_hpp_
 
 #include <GUI\GUIElement.hpp>
+#include <SFML\Text.hpp>
 
 #include <Utilities\Logger.hpp>
 #include <Utilities\Conversion.hpp>
@@ -10,15 +11,27 @@ class Label : GUIElement
 {
 private:
 	std::string text;
+	std::string font;
+	sf::Color textColor;
+	int textSize;
+	
+	struct Components {
+		Text text;
+	} components;
 public:
 	Label(AssetManager* _assets);
 	~Label();
 
+	void update();
 	void draw(sf::RenderTarget& target, sf::RenderStates states) const;
 	void handleEvent(sf::RenderWindow& target, sf::Event* _event);
 
 	void setText(std::string);
+	void setFont(std::string);
+	void setTextColor(sf::Color);
 	std::string getText();
+	std::string getFont();
+	sf::Color getTextColor();
 };
 
 #endif // !_Label_hpp_
