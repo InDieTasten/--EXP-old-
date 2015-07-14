0794378beea8b63cb24809665cd6b848c88edf71
InDieTasten <indietasten@gmail.com>
2015-07-14 12:51:11 +0200

    Delegate source update
    Former-commit-id: a639d8dffc3b9708bee3c2b62e2d34dc394b7be4 [formerly 5e466ac1c8a250d79211185c0712d03fb4901ad0]
    Former-commit-id: 941e0589a361eac92984d53ff48e2f9f333b4695

--- src/Input/Delegate.cpp
+++ src/Input/Delegate.cpp
@@ -1,3 +1,31 @@
 #include <Input\Delegate.hpp>
 
+template <class TReceiver>
+Delegate::Delegate()
+{
+	object = nullptr;
+	methPtr = nullptr;
+}
+template <class TReceiver>
+Delegate::Delegate(TReceiver* _receiver, void (TReceiver::* _methPtr)(EventPublisher*, TEventType))
+{
+	object = reinterpret_cast<Generic*>(_receiver);
+	methPtr = reinterpret_cast<void (Generic::*)(EventPublisher*, TEventType)>(_methPtr);
+}
 
+template <class TReceiver>
+void Delegate::setObject(TReceiver* _receiver)
+{
+	object = reinterpret_cast<Generic*>(_receiver);
+}
+
+template <class TReceiver>
+void Delegate::setMethod(void (TReceiver::* _methPtr)(EventPublisher*, TEventType))
+{
+	methPtr = reinterpret_cast<void (Generic::*)(EventPublisher*, TEventType)>(_methPtr);
+}
+
+void Delegate::operator()(EventPublisher* sender, TEventType eventArgs)
+{
+	(object->*methPtr)(sender, eventArgs);
+}
