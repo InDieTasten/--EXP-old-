df0bb7ff12430287f2c9ce85d5b555b982a5a285
InDieTasten <indietasten@gmail.com>
2015-07-16 07:55:58 +0200

    Actuator abstraction
    Former-commit-id: 09d470ea56cd797a2d23a6d6c3cfb9357b2d3f09

--- include/Input/Actuator.hpp
+++ include/Input/Actuator.hpp
@@ -10,70 +10,10 @@
 class Actuator
 {
 public:
-	enum InputType
-	{
-		Keyboard,
-		MouseButton,
-		MousePosition,
-		Joystick,
-		JoystickButton,
-		Magic
-	};
-	enum MouseAxis
-	{
-		X,
-		Y
-	};
-private:
-
-	EventManager* eventManager;
-
-	InputType inputType;
-
-	sf::Keyboard::Key key;
-	sf::Mouse::Button button;
-	MouseAxis mouseAxis;
-	unsigned int joystick;
-	unsigned int joyButton;
-	sf::Joystick::Axis axis;
-
-	float value;
-
-	bool enabled;
-
-	void listenermethod(EventPublisher* sender, sf::Event*);
-
-public:
 	Actuator();
-	Actuator(EventManager* _eventManager);
 	~Actuator();
 
-	bool getEnabled();
-
-
-	void detectNext();
-	void detectNext(InputType _filter);
-
-	void setEventManager(EventManager* _eventManager);
-	EventManager* getEventManager();
-
-	void setInputType(InputType _type);
-	void setKey(sf::Keyboard::Key _key);
-	void setButton(sf::Mouse::Button _button);
-	void setMouseAxis(MouseAxis _mouseAxis);
-	void setAxis(unsigned int _joystick, sf::Joystick::Axis _axis);
-	void setJoyButton(unsigned int _joystick, unsigned int _button);
-
-	InputType getInputType();
-	sf::Keyboard::Key getKey();
-	sf::Mouse::Button getButton();
-	MouseAxis getMouseAxis();
-	sf::Joystick::Axis getAxis();
-	unsigned int getJoystick();
-	unsigned int getJoyButton();
-
-	void setControlVector(float _value);
-	float getControlVector();
+	virtual float getControlVector() = 0;
 };
 
 
