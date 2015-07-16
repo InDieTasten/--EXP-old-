ac320cd69219afd77de4202a420ed4c124db203f
InDieTasten <indietasten@gmail.com>
2015-07-16 08:12:02 +0200

    Actuator base
    Former-commit-id: b26a0b4ff5d0817b2d5ee626ec9f6b35aced7e4f

--- src/Input/Actuator.cpp
+++ src/Input/Actuator.cpp
@@ -2,7 +2,8 @@
 
 Actuator::Actuator()
 {
-
+	multiplier = 1.0f;
+	adjustment = 0.0f;
 	EXP::log("[Info]Actuator has been constructed: " + utils::tostring(this));
 }
 Actuator::~Actuator()
@@ -10,3 +11,24 @@ Actuator::~Actuator()
 	EXP::log("[Info]Actuator has been destructed: " + utils::tostring(this));
 }
 
+float Actuator::getControlVector()
+{
+	return multiplier * getRawVector() + adjustment;
+}
+
+void Actuator::setMultiplier(float _value)
+{
+	multiplier = _value;
+}
+void Actuator::setAdjustment(float _value)
+{
+	adjustment = _value;
+}
+float Actuator::getMultiplier()
+{
+	return multiplier;
+}
+float Actuator::getAdjustment()
+{
+	return adjustment;
+}
