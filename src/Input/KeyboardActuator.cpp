1a47b024e100c2a0bb080817de35dc017201f4af
InDieTasten <indietasten@gmail.com>
2015-07-16 08:43:40 +0200

    KeyboardActuator source init
    Former-commit-id: d258b9c4e6a7c60e7d179911a04d9ac344cf2abb

---
+++ src/Input/KeyboardActuator.cpp
@@ -0,0 +1,60 @@
+#include <Input\KeyboardActuator.hpp>
+
+KeyboardActuator::KeyboardActuator(EventManager& _manager) : Actuator(), eventManager(_manager)
+{
+
+	EXP::log("[Info]KeyboardActuator has been constructed: " + utils::tostring(this));
+}
+KeyboardActuator::~KeyboardActuator()
+{
+
+	EXP::log("[Info]KeyboardActuator has been destructed: " + utils::tostring(this));
+}
+
+float KeyboardActuator::getRawVector()
+{
+	confmtx.lock();
+	float result = sf::Keyboard::isKeyPressed(code);
+	confmtx.unlock();
+	return result;
+}
+
+void KeyboardActuator::listen(EventPublisher* _sender, sf::Event::KeyEvent _event)
+{
+	confmtx.lock();
+	code = _event.code;
+	eventManager.removeKeyPress(Delegate<sf::Event::KeyEvent>(this, &KeyboardActuator::listen));
+	confmtx.unlock();
+}
+
+void KeyboardActuator::detectNext()
+{
+	eventManager.addKeyPress(Delegate<sf::Event::KeyEvent>(this, &KeyboardActuator::listen));
+}
+
+void KeyboardActuator::setKeyCode(sf::Keyboard::Key _key)
+{
+	confmtx.lock();
+	code = _key;
+	confmtx.unlock();
+}
+void KeyboardActuator::setEventManager(EventManager& _manager)
+{
+	confmtx.lock();
+	eventManager = _manager;
+	confmtx.unlock();
+}
+sf::Keyboard::Key KeyboardActuator::getKeyCode()
+{
+	confmtx.lock();
+	sf::Keyboard::Key result = code;
+	confmtx.unlock();
+	return result;
+}
+EventManager& KeyboardActuator::getEventManager()
+{
+	confmtx.lock();
+	EventManager& result = eventManager;
+	confmtx.unlock();
+	return result;
+}
