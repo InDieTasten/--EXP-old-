c582877d0544b5cc7b2a1a334e765fdd5da2a8e2
InDieTasten <indietasten@gmail.com>
2015-08-03 01:48:13 +0200

    PilotSeat changes
    Former-commit-id: 35ba5682ff20e8fa108a3c32a569087447d9d589

--- include/Data/PilotSeat.hpp
+++ include/Data/PilotSeat.hpp
@@ -5,6 +5,7 @@
 #include <Data\Attachable.hpp>
 #include <Data\Taggable.hpp>
 #include <Input\Actuator.hpp>
+#include <Input\NullActuator.hpp>
 #include <Data\Thruster.hpp>
 #include <vector>
 
@@ -14,17 +15,34 @@ class PilotSeat : public Attachable, public Taggable
 private:
 	EventManager* eventManager;
 
-	Actuator translateForward;
-	Actuator translateBack;
-	Actuator translateLeft;
-	Actuator translateRight;
-	Actuator rotateLeft;
-	Actuator rotateRight;
-	Actuator primaryWeapon;
-	Actuator secondaryWeapon;
+	Actuator* translateForward;
+	Actuator* translateBack;
+	Actuator* translateLeft;
+	Actuator* translateRight;
+	Actuator* rotateLeft;
+	Actuator* rotateRight;
+	Actuator* primaryWeapon;
+	Actuator* secondaryWeapon;
 public:
 	PilotSeat(EventManager* _eventManager);
 	~PilotSeat();
+
+	void setTranslateForward(Actuator& _act);
+	void setTranslateBack(Actuator& _act);
+	void setTranslateLeft(Actuator& _act);
+	void setTranslateRight(Actuator& _act);
+	void setRotateLeft(Actuator& _act);
+	void setRotateRight(Actuator& _act);
+	void setPrimaryWeapon(Actuator& _act);
+	void setSecondaryWeapon(Actuator& _act);
+	Actuator& getTranslateForward();
+	Actuator& getTranslateBack();
+	Actuator& getTranslateLeft();
+	Actuator& getTranslateRight();
+	Actuator& getRotateLeft();
+	Actuator& getRotateRight();
+	Actuator& getPrimaryWeapon();
+	Actuator& getSecondaryWeapon();
 
 	void setThrottle(std::vector<Thruster>& _thrusters); //will return the current input vector for the thruster given
 };
